d5d3bcefd90db36637e455405f7d89e1cd1aafa8
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2019-08-23 16:22:29 -0400

    Add gitignore to openapi ignore
    Update .apigentools-info

---
+++ datadog_v1/test_utils.go
@@ -0,0 +1,30 @@
+package datadog_v1
+
+import (
+	"context"
+	"os"
+	"testing"
+)
+
+// TESTAPICLIENT is the api client to use for tests
+var TESTAPICLIENT *APIClient
+
+// TESTAUTH is the authentication context to use with each test API call
+var TESTAUTH context.Context
+
+func setupTest(t *testing.T) func(t *testing.T) {
+	// SETUP testing
+	TESTAUTH = context.WithValue(
+		context.Background(),
+		ContextAPIKey,
+		APIKey{
+			Key:    os.Getenv("DD_TEST_CLIENT_API_KEY"),
+			AppKey: os.Getenv("DD_TEST_CLIENT_APP_KEY"),
+		},
+	)
+	config := NewConfiguration()
+	TESTAPICLIENT = NewAPIClient(config)
+	return func(t *testing.T) {
+		// TEARDOWN testing
+	}
+}
