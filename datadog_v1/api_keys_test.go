f3f058eb5509c78ac1b0fe2bf4da18df3ae71afa
Raymond You <raymond.you@datadoghq.com>
2019-11-12 17:41:10 -0500

    x

--- datadog_v1/api_keys_test.go
+++ datadog_v1/api_keys_test.go
@@ -2,9 +2,9 @@ package datadog_v1_test
 
 import (
 	"encoding/json"
-	"fmt"
-	"os"
-	"reflect"
+//	"fmt"
+	// "os"
+	// "reflect"
 	"testing"
 
 	datadog "github.com/DataDog/datadog-api-client-go/datadog_v1"
@@ -13,21 +13,116 @@ import (
 	"gotest.tools/assert"
 )
 
-func TestGetApiKeys(t *testing.T) {
+func TestCreateApiKey(t *testing.T) {
 	// Setup the Client we'll use to interact with the Test account
 	teardownTest := setupUnitTest(t)
 	defer teardownTest(t)
 	defer gock.Off()
 
 	// Setup fixture data
-	var apiKeyListFixture datadog.ApiKeyListResponse
-	json.Unmarshal(setupGock(t, "keys/api_keys_get.json", "get", "/api_key"), &apiKeyListFixture)
-	apiKeyListFixture := apiKeyListFixture.GetApiKeys()[0]
+	var apiKeyResponse datadog.ApiKeyResponse
+	json.Unmarshal(setupGock(t, "keys/api_key_get.json", "delete", "/api_key/"), &apiKeyResponse)
+	apiKey := apiKeyResponse.GetApiKey()
 
 	// Get mocked request data
-	apiKeys, _, err := TESTAPICLIENT.KeysApi.GetApiKeys(TESTAUTH)
-	if err != nil {
-		t.Errorf("Failed to Get the test org %s", err)
-	}
+	var createOpts datadog.CreateAPIKeyOpts
+	apiKeySettings := *apiKeyResponse.GetApiKey().Name
+	createOpts.ApiKey = optional.NewInterface(datadog.ApiKey{Name: &apiKeySettings})
+	apiKeyResp, _, _ := TESTAPICLIENT.KeysApi.CreateAPIKey(TESTAUTH, &createOpts)
 
+	apiKeyReturned := apiKeyResp.GetApiKey()
+	assert.Equal(t, apiKey.GetName(), apiKeyReturned.GetName())
+	assert.Equal(t, apiKey.GetKey(), apiKeyReturned.GetKey())
 }
+
+func TestGetApiKey(t *testing.T) {
+	// Setup the Client we'll use to interact with the Test account
+	teardownTest := setupUnitTest(t)
+	defer teardownTest(t)
+	defer gock.Off()
+
+	// Setup fixture data
+	var apiKeyResponse datadog.ApiKeyResponse
+	json.Unmarshal(setupGock(t, "keys/api_key_get.json", "get", "/api_key"), &apiKeyResponse)
+	apiKey := apiKeyResponse.GetApiKey()
+
+	// Get mocked request data
+	apiKeyResponseReturned, _, _ := TESTAPICLIENT.KeysApi.GetAPIKey(TESTAUTH, "b65bee9d3578485794a5b6069d73dfb7")
+
+	apiKeyReturned := apiKeyResponseReturned.GetApiKey()
+	assert.Equal(t, apiKey.GetCreated(), apiKeyReturned.GetCreated())
+	assert.Equal(t, apiKey.GetCreatedBy(), apiKeyReturned.GetCreatedBy())
+	assert.Equal(t, apiKey.GetKey(), apiKeyReturned.GetKey())
+	assert.Equal(t, apiKey.GetName(), apiKeyReturned.GetName())
+}
+
+func TestGetListApiKey(t *testing.T) {
+	// Setup the Client we'll use to interact with the Test account
+	teardownTest := setupUnitTest(t)
+	defer teardownTest(t)
+	defer gock.Off()
+
+	// Setup fixture data
+	var listApiKeyResponse datadog.ApiKeyListResponse
+	json.Unmarshal(setupGock(t, "keys/api_key_list_get.json", "get", "/api_key"), &listApiKeyResponse)
+	listApiKey := listApiKeyResponse.GetApiKeys()
+	singleApiKey := listApiKey[0]
+
+	// Get mocked request data
+	listApiKeyResponseReturned, _, _ := TESTAPICLIENT.KeysApi.GetAllAPIKeys(TESTAUTH)
+
+	listApiKeyReturned := listApiKeyResponseReturned.GetApiKeys()
+	assert.Equal(t, len(listApiKeyReturned), 2)
+	
+	singleApiKeyReturned := listApiKeyReturned[0]
+
+	assert.Equal(t, singleApiKeyReturned.GetCreated(), singleApiKey.GetCreated())
+	assert.Equal(t, singleApiKeyReturned.GetCreatedBy(), singleApiKey.GetCreatedBy())
+	assert.Equal(t, singleApiKeyReturned.GetKey(), singleApiKey.GetKey())
+	assert.Equal(t, singleApiKeyReturned.GetName(), singleApiKey.GetName())
+}
+
+func TestDeleteApiKey(t *testing.T) {
+	// Setup the Client we'll use to interact with the Test account
+	teardownTest := setupUnitTest(t)
+	defer teardownTest(t)
+	defer gock.Off()
+
+	// Setup fixture data
+	var apiKeyResponse datadog.ApiKeyResponse
+	json.Unmarshal(setupGock(t, "keys/api_key_get.json", "get", "/api_key"), &apiKeyResponse)
+	apiKey := apiKeyResponse.GetApiKey()
+
+	// Get mocked request data
+	apiKeyResponseReturned, _, _ := TESTAPICLIENT.KeysApi.DeleteAPIKey(TESTAUTH, "b65bee9d3578485794a5b6069d73dfb7")
+
+	apiKeyReturned := apiKeyResponseReturned.GetApiKey()
+	assert.Equal(t, apiKey.GetCreated(), apiKeyReturned.GetCreated())
+	assert.Equal(t, apiKey.GetCreatedBy(), apiKeyReturned.GetCreatedBy())
+	assert.Equal(t, apiKey.GetKey(), apiKeyReturned.GetKey())
+	assert.Equal(t, apiKey.GetName(), apiKeyReturned.GetName())
+}
+
+// func TestEditApiKey(t *testing.T) {
+// 	// Setup the Client we'll use to interact with the Test account
+// 	teardownTest := setupUnitTest(t)
+// 	defer teardownTest(t)
+// 	defer gock.Off()
+
+// 	// Setup fixture data
+// 	apiKeyId := "b65bee9d3578485794a5b6069d73dfb7"
+// 	var apiKeyResponse datadog.ApiKeyResponse
+// 	json.Unmarshal(setupGock(t, "keys/api_key_get.json", "delete", fmt.Sprintf("/api_key/%s", apiKeyId)), &apiKeyResponse)
+// 	apiKey := apiKeyResponse.GetApiKey()
+
+// 	// Get mocked request data
+// 	var updateOpts datadog.EditAPIKeyOpts
+// 	updateOpts = optional.NewInterface(datadog.EditAPIKeyOpts{ApiKey: apiKey})
+// 	apiKeyResponseReturned, _, err := TESTAPICLIENT.KeysApi.EditAPIKey(TESTAUTH, "b65bee9d3578485794a5b6069d73dfb7", &updateOpts)
+
+// 	apiKeyReturned := apiKeyResponseReturned.GetApiKey()
+// 	assert.Equal(t, apiKey.GetCreated(), apiKeyReturned.GetCreated())
+// 	assert.Equal(t, apiKey.GetCreatedBy(), apiKeyReturned.GetCreatedBy())
+// 	assert.Equal(t, apiKey.GetKey(), apiKeyReturned.GetKey())
+// 	assert.Equal(t, apiKey.GetName(), apiKeyReturned.GetName())
+// }
