4ad9a8897a7f142c78db1fb580d26af334e89293
Dmytro Milinevskyi <dmilinevskyi@gmail.com>
2019-11-15 13:22:28 +0100

    check run unit tests

---
+++ datadog_v1/api_check_runs_test.go
@@ -0,0 +1,55 @@
+package datadog_v1_test
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	datadog "github.com/DataDog/datadog-api-client-go/datadog_v1"
+	"gotest.tools/assert"
+)
+
+func TestSubmitCheckRuns(t *testing.T) {
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var checks []datadog.CheckRun
+		decoder := json.NewDecoder(r.Body)
+		err := decoder.Decode(&checks)
+		if err != nil {
+			w.Header().Add("Content-Type", "application/json")
+			w.WriteHeader(400)
+			fmt.Fprintf(w, `{"errors": ["Decode error: %s"]}`, err)
+			return
+		}
+		for _, check := range checks {
+			if check.Check == "" {
+				w.Header().Add("Content-Type", "application/json")
+				w.WriteHeader(400)
+				fmt.Fprintln(w, `{"errors": ["Missing field: check"]}`)
+				return
+			}
+		}
+		w.Header().Add("Content-Type", "application/json")
+		fmt.Fprint(w, `{"status": "ok"}`)
+	}))
+	defer srv.Close()
+	config := datadog.NewConfiguration()
+	config.BasePath = srv.URL
+	client := datadog.NewAPIClient(config)
+
+	submitCRResp, _, err := client.CheckRunsApi.SubmitCheckRuns(context.TODO(), []datadog.CheckRun{
+		datadog.CheckRun{
+			Check: "app.up",
+		},
+	})
+	assert.NilError(t, err)
+	assert.Equal(t, "ok", submitCRResp.GetStatus())
+
+	submitCRResp, _, err = client.CheckRunsApi.SubmitCheckRuns(context.TODO(), []datadog.CheckRun{
+		datadog.CheckRun{},
+	})
+	respErr := err.(datadog.GenericOpenAPIError).Model().(datadog.Error400)
+	assert.Equal(t, "Missing field: check", respErr.GetErrors()[0])
+}
