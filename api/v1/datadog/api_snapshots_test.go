af6068bae07e55ee7b5d77c5b33c8d2985052997
Jiri Kuncar <jiri.kuncar@gmail.com>
2019-11-29 11:52:28 +0100

    Test real graph snapshot

--- api/v1/datadog/api_snapshots_test.go
+++ api/v1/datadog/api_snapshots_test.go
@@ -2,6 +2,7 @@ package datadog_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/DataDog/datadog-api-client-go/api/v1/datadog"
 	"gotest.tools/assert"
@@ -11,13 +12,14 @@ func TestCreateGraphSnapshot(t *testing.T) {
 	teardownTest := setupTest(t)
 	defer teardownTest(t)
 
+	start := time.Now().Unix()
+	end := start + 24*60*60
 	testGraphSnapshot := datadog.GraphSnapshot{
-		GraphDef:    datadog.PtrString("Graph definition"),
-		MetricQuery: datadog.PtrString("Metrics query"),
-		SnapshotUrl: datadog.PtrString("Snapshot url"),
+		GraphDef:    datadog.PtrString(`{"requests": [{"q": "system.load.1{*}"}]}`),
+		MetricQuery: datadog.PtrString("system.load.1{*}"),
 	}
 
-	snapshot, httpresp, err := TESTAPICLIENT.SnapshotsApi.CreateGraphSnapshot(TESTAUTH, testGraphSnapshot)
+	snapshot, httpresp, err := TESTAPICLIENT.SnapshotsApi.GetGraphSnapshot(TESTAUTH, *testGraphSnapshot.MetricQuery, start, end, nil)
 	if err != nil {
 		t.Errorf("Error creating Snapshot %v: Status: %s: %v", testGraphSnapshot, err.(datadog.GenericOpenAPIError).Body(), err)
 	}
@@ -25,5 +27,5 @@ func TestCreateGraphSnapshot(t *testing.T) {
 
 	assert.Equal(t, snapshot.GetGraphDef(), testGraphSnapshot.GetGraphDef())
 	assert.Equal(t, snapshot.GetMetricQuery(), testGraphSnapshot.GetMetricQuery())
-	assert.Equal(t, snapshot.GetSnapshotUrl(), testGraphSnapshot.GetSnapshotUrl())
+	assert.Assert(t, snapshot.GetSnapshotUrl() != "")
 }
