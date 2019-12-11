4056ccfabfddeb21f638f9c726516e67f43cc5b0
Jiri Kuncar <jiri.kuncar@gmail.com>
2019-12-11 13:53:48 +0100

    Generate unique test users

--- api/v1/datadog/api_users_test.go
+++ api/v1/datadog/api_users_test.go
@@ -1,18 +1,25 @@
 package datadog_test
 
 import (
+	"fmt"
 	"log"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/DataDog/datadog-api-client-go/api/v1/datadog"
 	"gotest.tools/assert"
 )
 
-var TESTUSER = datadog.User{
-	Name:       datadog.PtrString("test user"),
-	Email:      datadog.PtrString("test@example.com"),
-	Handle:     datadog.PtrString("test@example.com"),
-	AccessRole: datadog.PtrString("ro"),
+func generateUniqueUser(t *testing.T) datadog.User {
+	prefix := fmt.Sprintf("%s-%d", t.Name(), time.Now().UnixNano())
+	email := strings.ToLower(prefix) + "@example.com"
+	return datadog.User{
+		Name:       datadog.PtrString(prefix),
+		Email:      datadog.PtrString(email),
+		Handle:     datadog.PtrString(email),
+		AccessRole: datadog.PtrString("ro"),
+	}
 }
 
 // [TODO] You can't update another user's email
@@ -23,61 +30,61 @@ var UPDATEUSER = datadog.User{
 	AccessRole: datadog.PtrString("st"),
 }
 
-var UPDATEENABLEUSER = datadog.User{
-	Disabled: datadog.PtrBool(false),
-}
-
 func TestCreateUser(t *testing.T) {
 	// Setup the Client we'll use to interact with the Test account
 	teardownTest := setupTest(t)
 	defer teardownTest(t)
-	defer disableUser(TESTUSER.GetHandle())
+
+	testUser := generateUniqueUser(t)
+	defer disableUser(testUser.GetHandle())
 
 	// Assert User Created with proper fields
-	userCreateResponse, httpresp, err := TESTAPICLIENT.UsersApi.CreateUser(TESTAUTH, TESTUSER)
+	userCreateResponse, httpresp, err := TESTAPICLIENT.UsersApi.CreateUser(TESTAUTH, testUser)
 	if err != nil {
 		t.Errorf("Error Creating User: %v", err)
 	}
-	// FIXME assert.Equal(t, httpresp.StatusCode, 200)
+	assert.Equal(t, httpresp.StatusCode, 200)
 
 	user := userCreateResponse.GetUser()
-	assert.Equal(t, user.GetName(), TESTUSER.GetName())
-	assert.Equal(t, user.GetHandle(), TESTUSER.GetHandle())
-	assert.Equal(t, user.GetAccessRole(), TESTUSER.GetAccessRole())
-	assert.Equal(t, user.GetEmail(), TESTUSER.GetEmail())
+	assert.Equal(t, user.GetName(), testUser.GetName())
+	assert.Equal(t, user.GetHandle(), testUser.GetHandle())
+	assert.Equal(t, user.GetAccessRole(), testUser.GetAccessRole())
+	assert.Equal(t, user.GetEmail(), testUser.GetEmail())
 
 	// Assert User Get with proper fields
-	userGetResponse, httpresp, err := TESTAPICLIENT.UsersApi.GetUser(TESTAUTH, TESTUSER.GetEmail())
+	userGetResponse, httpresp, err := TESTAPICLIENT.UsersApi.GetUser(TESTAUTH, testUser.GetEmail())
 	if err != nil {
-		t.Errorf("Error Getting User %s: Response %s: %v", TESTUSER.GetEmail(), err.(datadog.GenericOpenAPIError).Body(), err)
+		t.Errorf("Error Getting User %s: Response %s: %v", testUser.GetEmail(), err.(datadog.GenericOpenAPIError).Body(), err)
 	}
 	assert.Equal(t, httpresp.StatusCode, 200)
 
 	user = userGetResponse.GetUser()
-	assert.Equal(t, user.GetName(), TESTUSER.GetName())
-	assert.Equal(t, user.GetHandle(), TESTUSER.GetHandle())
-	assert.Equal(t, user.GetAccessRole(), TESTUSER.GetAccessRole())
-	assert.Equal(t, user.GetEmail(), TESTUSER.GetEmail())
+	assert.Equal(t, user.GetName(), testUser.GetName())
+	assert.Equal(t, user.GetHandle(), testUser.GetHandle())
+	assert.Equal(t, user.GetAccessRole(), testUser.GetAccessRole())
+	assert.Equal(t, user.GetEmail(), testUser.GetEmail())
 }
 
 func TestUpdateUser(t *testing.T) {
 	// Setup the Client we'll use to interact with the Test account
 	teardownTest := setupTest(t)
 	defer teardownTest(t)
-	defer disableUser(TESTUSER.GetHandle())
+
+	testUser := generateUniqueUser(t)
+	defer disableUser(testUser.GetHandle())
 
 	// Assert User Created with proper fields
-	userCreateResponse, httpresp, err := TESTAPICLIENT.UsersApi.CreateUser(TESTAUTH, TESTUSER)
+	userCreateResponse, httpresp, err := TESTAPICLIENT.UsersApi.CreateUser(TESTAUTH, testUser)
 	if err != nil {
-		t.Errorf("Error creating User %v: Response %s: %v", TESTUSER, err.(datadog.GenericOpenAPIError).Body(), err)
+		t.Errorf("Error creating User %v: Response %s: %v", testUser, err.(datadog.GenericOpenAPIError).Body(), err)
 	}
-	// FIXME assert.Equal(t, httpresp.StatusCode, 200)
+	assert.Equal(t, httpresp.StatusCode, 200)
 	userCreateResponse.GetUser()
 
 	// Assert User Get with proper fields
-	userUpdateResponse, httpresp, err := TESTAPICLIENT.UsersApi.UpdateUser(TESTAUTH, TESTUSER.GetHandle(), UPDATEUSER)
+	userUpdateResponse, httpresp, err := TESTAPICLIENT.UsersApi.UpdateUser(TESTAUTH, testUser.GetHandle(), UPDATEUSER)
 	if err != nil {
-		t.Errorf("Error getting User %s: Response %s: %v", TESTUSER.GetHandle(), err.(datadog.GenericOpenAPIError).Body(), err)
+		t.Errorf("Error getting User %s: Response %s: %v", testUser.GetHandle(), err.(datadog.GenericOpenAPIError).Body(), err)
 	}
 	assert.Equal(t, httpresp.StatusCode, 200)
 
@@ -87,18 +94,24 @@ func TestUpdateUser(t *testing.T) {
 	assert.Equal(t, user.GetDisabled(), UPDATEUSER.GetDisabled())
 	assert.Equal(t, user.GetAccessRole(), UPDATEUSER.GetAccessRole())
 	// Test unchanged field remains unchanged
-	assert.Equal(t, user.GetHandle(), TESTUSER.GetHandle())
+	assert.Equal(t, user.GetHandle(), testUser.GetHandle())
 }
 
 func TestDisableUser(t *testing.T) {
 	// We already test this in the disableUser cleanup function, but good to have an explicit test
+	testUser := generateUniqueUser(t)
+	defer disableUser(testUser.GetHandle())
 
-	// First ensure that the user isn't currently disabled
-	TESTAPICLIENT.UsersApi.UpdateUser(TESTAUTH, TESTUSER.GetHandle(), UPDATEENABLEUSER)
-
-	_, httpresp, err := TESTAPICLIENT.UsersApi.DisableUser(TESTAUTH, TESTUSER.GetHandle())
+	// Assert User Created with proper fields
+	_, httpresp, err := TESTAPICLIENT.UsersApi.CreateUser(TESTAUTH, testUser)
 	if err != nil {
-		t.Errorf("Error disabling User %s: Response %s: %v", TESTUSER.GetHandle(), err.(datadog.GenericOpenAPIError).Body(), err)
+		t.Errorf("Error Creating User: %v", err)
+	}
+	assert.Equal(t, httpresp.StatusCode, 200)
+
+	_, httpresp, err = TESTAPICLIENT.UsersApi.DisableUser(TESTAUTH, testUser.GetHandle())
+	if err != nil {
+		t.Errorf("Error disabling User %s: Response %s: %v", testUser.GetHandle(), err.(datadog.GenericOpenAPIError).Body(), err)
 	}
 	assert.Equal(t, httpresp.StatusCode, 200)
 }
