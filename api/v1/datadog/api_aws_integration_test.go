c49710d5d3ba58b8bd83b7f17a6fc2766b10943e
Jiri Kuncar <jiri.kuncar@datadoghq.com>
2019-12-13 08:39:33 +0100

    Generate unique AWS accounts (#40)

--- api/v1/datadog/api_aws_integration_test.go
+++ api/v1/datadog/api_aws_integration_test.go
@@ -1,26 +1,30 @@
 package datadog_test
 
 import (
+	"fmt"
 	"log"
 	"reflect"
 	"testing"
+	"time"
 
 	"github.com/DataDog/datadog-api-client-go/api/v1/datadog"
 	"github.com/antihax/optional"
 	"gotest.tools/assert"
 )
 
-var TESTAWSACC = datadog.AwsAccount{
-	AccountId:                     datadog.PtrString("123456789012"),
-	RoleName:                      datadog.PtrString("DatadogAWSIntegrationRole"),
-	AccountSpecificNamespaceRules: &map[string]bool{"opsworks": true},
-	FilterTags:                    &[]string{"testTag", "test:Tag2"},
-	HostTags:                      &[]string{"filter:one", "filtertwo"},
+func generateUniqueAwsAccount() datadog.AwsAccount {
+	accountId := fmt.Sprintf("go_%09d", time.Now().UnixNano()%1000000000)
+	return datadog.AwsAccount{
+		AccountId:                     &accountId,
+		RoleName:                      datadog.PtrString("DatadogAWSIntegrationRole"),
+		AccountSpecificNamespaceRules: &map[string]bool{"opsworks": true},
+		FilterTags:                    &[]string{"testTag", "test:Tag2"},
+		HostTags:                      &[]string{"filter:one", "filtertwo"},
+	}
 }
 
 var TESTUPDATEAWSACC = datadog.AwsAccount{
-	AccountId:                     datadog.PtrString("123456789012"),
-	RoleName:                      datadog.PtrString("DatadogAWSIntegrationRole"),
+	RoleName:                      datadog.PtrString("DatadogAWSIntegrationRoleUpdated"),
 	AccountSpecificNamespaceRules: &map[string]bool{"opsworks": false},
 	FilterTags:                    &[]string{"testTagUpdate", "testUpdated:Tag2"},
 	HostTags:                      &[]string{"filter:foo", "bar"},
@@ -30,58 +34,80 @@ func TestCreateAWSAccount(t *testing.T) {
 	// Setup the Client we'll use to interact with the Test account
 	teardownTest := setupTest(t)
 	defer teardownTest(t)
-	defer uninstallAWSIntegration(TESTAWSACC)
+
+	testAwsAccount := generateUniqueAwsAccount()
 
 	// Assert AWS Integration Created with proper fields
-	TESTAPICLIENT.AWSIntegrationApi.CreateAWSAccount(TESTAUTH, TESTAWSACC)
+	_, httpresp, err := TESTAPICLIENT.AWSIntegrationApi.CreateAWSAccount(TESTAUTH, testAwsAccount)
+	if err != nil {
+		t.Errorf("Error creating AWS Account: Response %s: %v", err.(datadog.GenericOpenAPIError).Body(), err)
+	}
+	assert.Equal(t, httpresp.StatusCode, 200)
+	defer uninstallAWSIntegration(testAwsAccount)
 
 	awsAcctOpts := datadog.GetAllAWSAccountsOpts{
-		AccountId: optional.NewString(TESTAWSACC.GetAccountId()),
-		RoleName:  optional.NewString(TESTAWSACC.GetRoleName()),
+		AccountId: optional.NewString(testAwsAccount.GetAccountId()),
+		RoleName:  optional.NewString(testAwsAccount.GetRoleName()),
 	}
-	awsAccts, _, err := TESTAPICLIENT.AWSIntegrationApi.GetAllAWSAccounts(TESTAUTH, &awsAcctOpts)
+	awsAccts, httpresp, err := TESTAPICLIENT.AWSIntegrationApi.GetAllAWSAccounts(TESTAUTH, &awsAcctOpts)
 	if err != nil {
-		t.Errorf("Error Getting AWS Account: %v", err)
+		t.Errorf("Error getting AWS Account: Response %s: %v", err.(datadog.GenericOpenAPIError).Body(), err)
 	}
-	awsAcct := awsAccts.GetAccounts()[0]
-	assert.Equal(t, awsAcct.GetAccountId(), TESTAWSACC.GetAccountId())
-	assert.Equal(t, awsAcct.GetRoleName(), TESTAWSACC.GetRoleName())
+	assert.Equal(t, httpresp.StatusCode, 200)
+
+	awsAccounts := awsAccts.GetAccounts()
+	assert.Assert(t, len(awsAccounts) > 0, "No AWS accounts found")
+
+	awsAcct := awsAccounts[0]
+	assert.Equal(t, awsAcct.GetAccountId(), testAwsAccount.GetAccountId())
+	assert.Equal(t, awsAcct.GetRoleName(), testAwsAccount.GetRoleName())
 	// Golang doesn't have an equality operator defined for slices
-	assert.Equal(t, reflect.DeepEqual(awsAcct.GetHostTags(), TESTAWSACC.GetHostTags()), true)
-	assert.Equal(t, reflect.DeepEqual(awsAcct.GetFilterTags(), TESTAWSACC.GetFilterTags()), true)
-	assert.Equal(t, reflect.DeepEqual(awsAcct.GetAccountSpecificNamespaceRules(), TESTAWSACC.GetAccountSpecificNamespaceRules()), true)
+	assert.Equal(t, reflect.DeepEqual(awsAcct.GetHostTags(), testAwsAccount.GetHostTags()), true)
+	assert.Equal(t, reflect.DeepEqual(awsAcct.GetFilterTags(), testAwsAccount.GetFilterTags()), true)
+	assert.Equal(t, reflect.DeepEqual(awsAcct.GetAccountSpecificNamespaceRules(), testAwsAccount.GetAccountSpecificNamespaceRules()), true)
 }
 
 func TestUpdateAWSAccount(t *testing.T) {
 	// Setup the Client we'll use to interact with the Test account
 	teardownTest := setupTest(t)
 	defer teardownTest(t)
-	defer uninstallAWSIntegration(TESTAWSACC)
+
+	testAwsAccount := generateUniqueAwsAccount()
 
 	// Assert AWS Integration Created with proper fields
-	TESTAPICLIENT.AWSIntegrationApi.CreateAWSAccount(TESTAUTH, TESTAWSACC)
+	_, httpresp, err := TESTAPICLIENT.AWSIntegrationApi.CreateAWSAccount(TESTAUTH, testAwsAccount)
+	if err != nil {
+		t.Errorf("Error creating AWS Account: Response %s: %v", err.(datadog.GenericOpenAPIError).Body(), err)
+	}
+	assert.Equal(t, httpresp.StatusCode, 200)
+	defer uninstallAWSIntegration(testAwsAccount)
 
 	awsAcctOpts := datadog.UpdateAWSAccountOpts{
-		AccountId: optional.NewString(TESTAWSACC.GetAccountId()),
-		RoleName:  optional.NewString(TESTAWSACC.GetRoleName()),
+		AccountId: optional.NewString(testAwsAccount.GetAccountId()),
+		RoleName:  optional.NewString(testAwsAccount.GetRoleName()),
 	}
-	_, _, err := TESTAPICLIENT.AWSIntegrationApi.UpdateAWSAccount(TESTAUTH, TESTUPDATEAWSACC, &awsAcctOpts)
+	_, httpresp, err = TESTAPICLIENT.AWSIntegrationApi.UpdateAWSAccount(TESTAUTH, TESTUPDATEAWSACC, &awsAcctOpts)
 	if err != nil {
 		t.Errorf("Error Updating AWS Account: %v", err)
 	}
+	assert.Equal(t, httpresp.StatusCode, 200)
 
 	// Assert AWS Account Get with proper fields
 	awsGetAcctOpts := datadog.GetAllAWSAccountsOpts{
-		AccountId: optional.NewString(TESTAWSACC.GetAccountId()),
-		RoleName:  optional.NewString(TESTAWSACC.GetRoleName()),
+		AccountId: optional.NewString(testAwsAccount.GetAccountId()),
+		RoleName:  optional.NewString(TESTUPDATEAWSACC.GetRoleName()),
 	}
 	awsAccts, _, err := TESTAPICLIENT.AWSIntegrationApi.GetAllAWSAccounts(TESTAUTH, &awsGetAcctOpts)
 	if err != nil {
 		t.Errorf("Error Getting AWS Account: %v", err)
 	}
-	awsAcct := awsAccts.GetAccounts()[0]
+	assert.Equal(t, httpresp.StatusCode, 200)
+
+	awsAccounts := awsAccts.GetAccounts()
+	assert.Assert(t, len(awsAccounts) > 0, "No AWS accounts found")
+
+	awsAcct := awsAccounts[0]
 	// Test fields were updated
-	assert.Equal(t, awsAcct.GetAccountId(), TESTUPDATEAWSACC.GetAccountId())
 	assert.Equal(t, awsAcct.GetRoleName(), TESTUPDATEAWSACC.GetRoleName())
 	// Golang doesn't have an equality operator defined for slices
 	assert.Equal(t, reflect.DeepEqual(awsAcct.GetHostTags(), TESTUPDATEAWSACC.GetHostTags()), true)
@@ -91,11 +117,17 @@ func TestUpdateAWSAccount(t *testing.T) {
 
 func TestDisableAWSAcct(t *testing.T) {
 	// We already test this in the disableAWSAccount cleanup function, but good to have an explicit test
+	testAwsAccount := generateUniqueAwsAccount()
 
 	// Lets first create the account of us to delete
-	TESTAPICLIENT.AWSIntegrationApi.CreateAWSAccount(TESTAUTH, TESTAWSACC)
+	_, httpresp, err := TESTAPICLIENT.AWSIntegrationApi.CreateAWSAccount(TESTAUTH, testAwsAccount)
+	if err != nil {
+		t.Errorf("Error creating AWS Account: Response %s: %v", err.(datadog.GenericOpenAPIError).Body(), err)
+	}
+	assert.Equal(t, httpresp.StatusCode, 200)
+	// we are testing the "defer uninstallAWSIntegration(testAwsAccount)" bellow
 
-	_, httpresp, err := TESTAPICLIENT.AWSIntegrationApi.DeleteAWSAccount(TESTAUTH, TESTAWSACC)
+	_, httpresp, err = TESTAPICLIENT.AWSIntegrationApi.DeleteAWSAccount(TESTAUTH, testAwsAccount)
 	if err != nil {
 		t.Errorf("Error disabling AWS Account: Response %s: %v", err.(datadog.GenericOpenAPIError).Body(), err)
 	}
