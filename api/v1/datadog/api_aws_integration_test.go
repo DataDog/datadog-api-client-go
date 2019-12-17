9637972b0c4b20d435cece7dbddc80e86ddd5b22
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2019-12-17 16:35:40 -0500

    Remove log lines

--- api/v1/datadog/api_aws_integration_test.go
+++ api/v1/datadog/api_aws_integration_test.go
@@ -141,14 +141,11 @@ func TestDisableAWSAcct(t *testing.T) {
 
 func TestGenerateNewExternalId(t *testing.T) {
 	testAwsAccount := generateUniqueAwsAccount()
-	fmt.Printf("HEY THERE WE'RE TESTING")
 	// Lets first create the account for us to generate a new id against
 	_, httpresp, err := TESTAPICLIENT.AWSIntegrationApi.CreateAWSAccount(TESTAUTH, testAwsAccount)
-	fmt.Printf("HEY THERE WE'RE TESTING")
 	if err != nil || httpresp.StatusCode != 200 {
 		t.Fatalf("Error creating AWS Account: Response %s: %v", err.(datadog.GenericOpenAPIError).Body(), err)
 	}
-	fmt.Printf("HEY THERE WE'RE STILL TESTING")
 	apiResp, httpResp, err := TESTAPICLIENT.AWSIntegrationApi.GenerateNewAWSExternalID(TESTAUTH, testAwsAccount)
 	if err != nil {
 		t.Fatalf("Error generating new AWS External ID %v", err)
