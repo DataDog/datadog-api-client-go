bec4e8c5c67f6a683fa559d4534a023e93eedc30
Hippolyte HENRY <hippolyte.henry@datadoghq.com>
2020-01-09 11:58:08 +0100

    Add test for query

--- api/v1/datadog/api_metrics_test.go
+++ api/v1/datadog/api_metrics_test.go
@@ -108,7 +108,7 @@ func TestMetricSubmissionMock(t *testing.T) {
 	})
 }
 
-func TestMetricSubmissionIntegration(t *testing.T) {
+func TestMetrics(t *testing.T) {
 	teardownTest := setupTest(t)
 	defer teardownTest(t)
 
@@ -117,11 +117,16 @@ func TestMetricSubmissionIntegration(t *testing.T) {
 
 	testMetric := fmt.Sprintf("go.client.test.%d", now)
 	testPoints := [][]float64{{float64(now - 60), 10.5}, {float64(now), 11}}
+	testTags := []string{"tag:foo", "bar:baz"}
+	testHost := "go-client-test-host"
+	testQuery := fmt.Sprintf("avg:%s{bar:baz}by{host}", testMetric)
 	metricsPayload := datadog.MetricsPayload{
 		Series: &[]datadog.Series{
 			datadog.Series{
+				Host:   &testHost,
 				Metric: testMetric,
 				Points: testPoints,
+				Tags:   &testTags,
 			},
 		},
 	}
@@ -133,6 +138,7 @@ func TestMetricSubmissionIntegration(t *testing.T) {
 	assert.Equal(t, httpresp.StatusCode, 202)
 	assert.Equal(t, "ok", r.GetStatus())
 
+	// Check that the metric was submitted successfully
 	err = retry(func() bool {
 		metrics, httpresp, err := api.GetAllActiveMetrics(TESTAUTH).From(now).Execute()
 		if err != nil {
@@ -152,8 +158,31 @@ func TestMetricSubmissionIntegration(t *testing.T) {
 	}, 10, 10)
 
 	if err != nil {
-		t.Errorf("%v", err)
+		t.Fatalf("%v", err)
 	}
+
+	// Test query
+	queryResult, httpresp, err := api.QueryMetrics(TESTAUTH).From(now - 100).To(now + 100).Query(testQuery).Execute()
+	if err != nil {
+		t.Errorf("Error making query %s: Response %s: %v", testQuery, err.(datadog.GenericOpenAPIError).Body(), err)
+	}
+	assert.Equal(t, 200, httpresp.StatusCode)
+	assert.Equal(t, []string{"host"}, queryResult.GetGroupBy())
+	assert.Equal(t, testQuery, queryResult.GetQuery())
+	assert.Equal(t, (now-100)*1000, queryResult.GetFromDate())
+	assert.Equal(t, (now+100)*1000, queryResult.GetToDate())
+	assert.Equal(t, "ok", queryResult.GetStatus())
+	assert.Equal(t, "time_series", queryResult.GetResType())
+	assert.Equal(t, 1, len(queryResult.GetSeries()))
+	series := queryResult.GetSeries()[0]
+	assert.Equal(t, int64(2), series.GetLength())
+	assert.Equal(t, "avg", series.GetAggr())
+	assert.Equal(t, testMetric, series.GetDisplayName())
+	assert.Equal(t, testMetric, series.GetMetric())
+	assert.Equal(t, series.GetPointlist()[0][0], float64(series.GetStart()))
+	assert.Equal(t, series.GetPointlist()[1][0], float64(series.GetEnd()))
+	assert.Equal(t, 10.5, series.GetPointlist()[0][1])
+	assert.Equal(t, 11., series.GetPointlist()[1][1])
 }
 
 func TestMetricListActive(t *testing.T) {
