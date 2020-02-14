7444116a2f5735f22bc8d1b929b8cc47285df6ca
Hippolyte HENRY <hippolyte.henry@datadoghq.com>
2020-02-14 14:38:13 +0100

    Add header

--- tests/api/v1/datadog/api_service_checks_test.go
+++ tests/api/v1/datadog/api_service_checks_test.go
@@ -1,7 +1,7 @@
 /*
  * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
  * This product includes software developed at Datadog (https://www.datadoghq.com/).
- * Copyright 2020-Present Datadog, Inc.
+ * Copyright 2019-Present Datadog, Inc.
  */
 
 package test
--- tests/api/v1/datadog/api_usage_test.go
+++ tests/api/v1/datadog/api_usage_test.go
@@ -1,7 +1,7 @@
 /*
- * (C) Datadog, Inc. 2019
- * All rights reserved
- * Licensed under a 3-clause BSD style license (see LICENSE)
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-Present Datadog, Inc.
  */
 
 package test
