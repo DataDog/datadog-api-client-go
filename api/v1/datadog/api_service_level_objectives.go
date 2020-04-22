e0acb9e851c47c8425abbf615304e5bc8f5b8a77
ci.datadog-api-spec <packages@datadoghq.com>
2020-04-22 18:00:18 +0000

    Regenerate client from commit a9a8203 of spec repo

--- api/v1/datadog/api_service_level_objectives.go
+++ api/v1/datadog/api_service_level_objectives.go
@@ -1232,6 +1232,16 @@ func (r apiListSLOsRequest) Execute() (SLOListResponse, *_nethttp.Response, erro
 				return localVarReturnValue, localVarHTTPResponse, newErr
 			}
 			newErr.model = v
+			return localVarReturnValue, localVarHTTPResponse, newErr
+		}
+		if localVarHTTPResponse.StatusCode == 404 {
+			var v APIErrorResponse
+			err = r.apiService.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarReturnValue, localVarHTTPResponse, newErr
+			}
+			newErr.model = v
 		}
 		return localVarReturnValue, localVarHTTPResponse, newErr
 	}
