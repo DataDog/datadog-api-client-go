2ced67e8f1637541ecc380b71c0f00867a1afb39
Inigo Mediavilla <inigo.mediavilla@datadoghq.com>
2020-05-30 08:26:46 +0200

    [logs][archives] Purely mock based tests

--- tests/api/v2/datadog/api_logs_archives_test.go
+++ tests/api/v2/datadog/api_logs_archives_test.go
@@ -1,125 +1,140 @@
+package test
+
 /*
  * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
  * This product includes software developed at Datadog (https://www.datadoghq.com/).
  * Copyright 2019-Present Datadog, Inc.
  */
 
-package test
-
 import (
 	"context"
 	"fmt"
-	"gopkg.in/h2non/gock.v1"
-	"math/rand"
-	_nethttp "net/http"
-	"testing"
-
 	"github.com/DataDog/datadog-api-client-go/api/v2/datadog"
+	"github.com/DataDog/datadog-api-client-go/tests"
 	"github.com/stretchr/testify/assert"
+	"gopkg.in/h2non/gock.v1"
+	"reflect"
+	"testing"
 )
 
-const archiveName string = "datadog-api-client-go Tests Archive"
-const archiveBucket string = "dd-logs-test-datadog-api-client-go"
+// This test uses mocking because: 1) it relies on private data. 2) It relies on external services
 
-var archiveIntegration = datadog.LogsArchiveIntegrationS3{
-	AccountId: "711111111111",
-	RoleName:  "DatadogGoClientTestIntegrationRole",
+func TestLogsArchivesCreateS3(t *testing.T) {
+	archiveType := "s3"
+	action := "create"
+	testLogArchivesCreate(t, archiveType, action)
 }
-var archivePath = "/path/blou"
 
-const archiveQuery string = "service:toto"
+func TestLogsArchivesCreateAzure(t *testing.T) {
+	archiveType := "azure"
+	action := "create"
+	testLogArchivesCreate(t, archiveType, action)
+}
 
-func TestLogsArchivesLifecycle(t *testing.T) {
+func TestLogsArchivesCreateGCS(t *testing.T) {
+	archiveType := "gcs"
+	action := "create"
+	testLogArchivesCreate(t, archiveType, action)
+}
+
+func testLogArchivesCreate(t *testing.T, archiveType string, action string) {
+	if tests.GetRecording() == tests.ModeReplaying {
+		t.Skip("This test case does not support reply from recording")
+	}
 	ctx, finish := WithClient(WithFakeAuth(context.Background()), t)
-	defer finish()
 	client := Client(ctx)
-	config := client.GetConfig()
-	URL, err := config.ServerURLWithContext(ctx, "LogsArchivesTest.Lifecycle")
-	assert.NoError(t, err)
-	apiClient := apiClientTest{
-		ctx:    ctx,
-		client: client,
-	}
-	testClient := apiDummyClient{
-		archives: map[string]datadog.LogsArchive{},
-		client:   apiClient,
-		URL:      URL,
-	}
+	assert := tests.Assert(ctx, t)
+	defer finish()
+	inputArchive := datadog.NullableLogsArchiveCreateRequest{}
+	inputArchive.UnmarshalJSON([]byte(readFixture(t, fmt.Sprintf("fixtures/logs/archives/%s/in/%s.json", archiveType, action))))
+	outputArchive := datadog.NullableLogsArchive{}
+	outputArchive.UnmarshalJSON([]byte(readFixture(t, fmt.Sprintf("fixtures/logs/archives/%s/out/%s.json", archiveType, action))))
+	URL, err := client.GetConfig().ServerURLWithContext(ctx, fmt.Sprintf("LogsArchive.%s.%s", action, archiveType))
+	assert.NoError(err)
+	gock.New(URL).Post("/api/v2/logs/config/archives").JSON(inputArchive).Reply(200).JSON(outputArchive)
+	archivesAreEqual(t, *inputArchive.Get().Data.Attributes, *outputArchive.Get().Data.Attributes)
+	assert.Equal(outputArchive.Get().Data.Type, "archives")
+	r := reflect.ValueOf(outputArchive.Get().Data.Attributes.Destination.Get().GetActualInstance())
+	assert.Equal(reflect.Indirect(r).FieldByName("Type").String(), archiveType)
+	result, httpresp, err := client.LogsArchivesApi.CreateLogsArchive(ctx).Body(*inputArchive.Get()).Execute()
+	assert.Equal(httpresp.StatusCode, 200)
+	assert.Equal(result, *outputArchive.Get())
 	defer gock.Off()
-
-	archive := createArchive()
-	// 1. Create
-	createdArchive, httpresp, err := testClient.create(archive)
-	assertNoError(t, err, httpresp)
-	assert.Equal(t, 200, httpresp.StatusCode)
-	archivesAreEqual(t, *archive.Data.Attributes, *createdArchive.Data.Attributes)
-
-	// 2. Delete at the end
-	defer deleteLogsArchive(t, &testClient, *createdArchive.Data.Id)
-
-	// 3. Update by Id
-	archive.Data.Attributes.Query = "source:tata"
-	archiveUpdated, httpresp, err := testClient.updateById(*createdArchive.Data.Id, archive)
-	assert.NoError(t, err)
-	assert.Equal(t, "source:tata", archiveUpdated.Data.Attributes.Query)
-	archivesAreEqual(t, *archive.Data.Attributes, *archiveUpdated.Data.Attributes)
-
-	// 4. Get by Id
-	archiveGot, _, err := testClient.getById(*createdArchive.Data.Id)
-	assertNoError(t, err, httpresp)
-	archivesAreEqual(t, *archive.Data.Attributes, *archiveGot.Data.Attributes)
-	// 5. Get all
-	archives, _, err := testClient.getAll()
-	assertNoError(t, err, httpresp)
-	assert.True(t, len(*archives.Data) > 0)
-	lastArchive := (*archives.Data)[len(*archives.Data)-1]
-	archivesAreEqual(t, *archive.Data.Attributes, *lastArchive.Attributes)
 }
 
-func TestLogsArchivesNotFound(t *testing.T) {
+func TestLogsArchivesGetByID(t *testing.T) {
+	if tests.GetRecording() == tests.ModeReplaying {
+		t.Skip("This test case does not support reply from recording")
+	}
 	ctx, finish := WithClient(WithFakeAuth(context.Background()), t)
 	defer finish()
+	assert := tests.Assert(ctx, t)
 	client := Client(ctx)
-	config := client.GetConfig()
-	URL, err := config.ServerURLWithContext(ctx, "LogsArchivesTest.NotFound")
-	assert.NoError(t, err)
-	apiClient := apiClientTest{
-		ctx:    ctx,
-		client: client,
+	id := "XVlBzgbaiC"
+	action := "getbyid"
+	archiveType := "s3"
+	outputArchive := datadog.NullableLogsArchive{}
+	outputArchive.UnmarshalJSON([]byte(readFixture(t, fmt.Sprintf("fixtures/logs/archives/%s/out/%s.json", archiveType, action))))
+	URL, err := client.GetConfig().ServerURLWithContext(ctx, fmt.Sprintf("LogsArchive.%s.%s", action, archiveType))
+	assert.NoError(err)
+	gock.New(URL).Get(fmt.Sprintf("/api/v2/logs/config/archives/%s", id)).Reply(200).JSON(outputArchive)
+	result, httpresp, err := client.LogsArchivesApi.GetLogsArchive(ctx, id).Execute()
+	assert.Equal(httpresp.StatusCode, 200)
+	checkS3Archive(t, *outputArchive.Get().Data)
+	assert.Equal(result, *outputArchive.Get())
+	defer gock.Off()
+}
+
+func TestLogsArchivesDelete(t *testing.T) {
+	if tests.GetRecording() == tests.ModeReplaying {
+		t.Skip("This test case does not support reply from recording")
 	}
-	testClient := apiDummyClient{
-		archives: map[string]datadog.LogsArchive{},
-		client:   apiClient,
-		URL:      URL,
+	ctx, finish := WithClient(WithFakeAuth(context.Background()), t)
+	defer finish()
+	assert := tests.Assert(ctx, t)
+	id := "XVlBzgbaiC"
+	action := "deleteById"
+	archiveType := "s3"
+	client := Client(ctx)
+	URL, err := client.GetConfig().ServerURLWithContext(ctx, fmt.Sprintf("LogsArchive.%s.%s", action, archiveType))
+	assert.NoError(err)
+	gock.New(URL).Delete(fmt.Sprintf("/api/v2/logs/config/archives/%s", id)).Reply(204)
+	httpresp, err := client.LogsArchivesApi.DeleteLogsArchive(ctx, id).Execute()
+	assert.NoError(err)
+	assert.Equal(httpresp.StatusCode, 204)
+	defer gock.Off()
+}
+
+func TestLogsArchivesGetAll(t *testing.T) {
+	if tests.GetRecording() == tests.ModeReplaying {
+		t.Skip("This test case does not support reply from recording")
 	}
-	//defer gock.Off()
+	ctx, finish := WithClient(WithFakeAuth(context.Background()), t)
+	defer finish()
+	assert := tests.Assert(ctx, t)
+	action := "getall"
+	archiveType := "s3"
+	outputArchives := datadog.NullableLogsArchives{}
+	outputArchives.UnmarshalJSON([]byte(readFixture(t, fmt.Sprintf("fixtures/logs/archives/%s/out/%s.json", archiveType, action))))
+	URL, err := Client(ctx).GetConfig().ServerURLWithContext(ctx, fmt.Sprintf("LogsArchive.%s.%s", action, archiveType))
+	assert.NoError(err)
+	gock.New(URL).Get("/api/v2/logs/config/archives/").Reply(200).JSON(outputArchives)
+	assert.True(len(*outputArchives.Get().Data) > 0)
+	checkS3Archive(t, (*outputArchives.Get().Data)[0])
+	defer gock.Off()
+}
 
-	archive := createArchive()
-	// 1. Create
-	createdArchive, httpresp, err := testClient.create(archive)
-	if err != nil {
-		t.Fatalf("Error creating logs archive: Response: %v, Http Resp: %v", err, httpresp)
-	}
-	assert.Equal(t, 200, httpresp.StatusCode)
-	archivesAreEqual(t, *archive.Data.Attributes, *createdArchive.Data.Attributes)
-
-	// 2. Delete
-	deleteLogsArchive(t, &testClient, *createdArchive.Data.Id)
-
-	// 3. Get by Id
-	_, httpresp, err = testClient.getById(*createdArchive.Data.Id)
-	assert.True(t, err != nil)
-	assert.Equal(t, 404, httpresp.StatusCode)
-
-	// 4. Delete by Id
-	httpresp, err = testClient.deleteById(*createdArchive.Data.Id)
-	assert.True(t, err != nil)
-	assert.Equal(t, 404, httpresp.StatusCode)
-
-	// 5. Update by Id
-	_, httpresp, err = testClient.updateById(*createdArchive.Data.Id, archive)
-	assert.True(t, err != nil)
-	assert.Equal(t, 404, httpresp.StatusCode)
+func checkS3Archive(t *testing.T, outputArchive datadog.LogsArchiveDefinition) {
+	assert.Equal(t, outputArchive.Type, "archives")
+	destination := outputArchive.Attributes.Destination.Get().LogsArchiveDestinationS3
+	assert.Equal(t, destination.Type, datadog.LOGSARCHIVEDESTINATIONS3TYPE_S3)
+	assert.Equal(t, destination.Integration.AccountId, "711111111111")
+	assert.Equal(t, destination.Integration.RoleName, "DatadogGoClientTestIntegrationRole")
+	assert.Equal(t, destination.Path, datadog.PtrString("/path/blou"))
+	assert.Equal(t, destination.Bucket, "dd-logs-test-datadog-api-client-go")
+	assert.Equal(t, outputArchive.Attributes.Name, "datadog-api-client-go Tests Archive")
+	assert.Equal(t, outputArchive.Attributes.Query, "source:tata")
+	assert.Equal(t, outputArchive.Id, datadog.PtrString("XVlBzgbaiC"))
 }
 
 func archivesAreEqual(t *testing.T, archiveRequest datadog.LogsArchiveCreateRequestAttributes, createdArchive datadog.LogsArchiveAttributes) {
@@ -127,172 +142,19 @@ func archivesAreEqual(t *testing.T, archiveRequest datadog.LogsArchiveCreateRequ
 	assert.Equal(t, archiveRequest.Query, createdArchive.Query)
 	s3DestinationReq := archiveRequest.Destination.LogsArchiveDestinationS3
 	s3DestinationCreated := createdArchive.Destination.Get().LogsArchiveDestinationS3
-	assert.Equal(t, s3DestinationReq.Bucket, s3DestinationCreated.Bucket)
-	assert.Equal(t, s3DestinationReq.Path, s3DestinationCreated.Path)
-	assert.Equal(t, s3DestinationReq.Integration, s3DestinationCreated.Integration)
-	assert.Equal(t, s3DestinationReq.Type, s3DestinationCreated.Type)
+	assert.Equal(t, s3DestinationReq, s3DestinationCreated)
+	azureDestinationReq := archiveRequest.Destination.LogsArchiveDestinationAzure
+	azureDestinationCreated := createdArchive.Destination.Get().LogsArchiveDestinationAzure
+	assert.Equal(t, azureDestinationReq, azureDestinationCreated)
+	gcsDestinationReq := archiveRequest.Destination.LogsArchiveDestinationGCS
+	gcsDestinationCreated := createdArchive.Destination.Get().LogsArchiveDestinationGCS
+	assert.Equal(t, gcsDestinationReq, gcsDestinationCreated)
 }
 
-func deleteLogsArchive(t *testing.T, testClient logsArchiveTestClient, id string) {
-	httpresp, err := testClient.deleteById(id)
-	assertNoError(t, err, httpresp)
-	assert.Equal(t, 204, httpresp.StatusCode)
-}
-
-func createArchive() datadog.LogsArchiveCreateRequest {
-	destinationS3 := datadog.LogsArchiveDestinationS3{
-		Bucket:      archiveBucket,
-		Integration: archiveIntegration,
-		Path:        &archivePath,
-		Type:        datadog.LOGSARCHIVEDESTINATIONS3TYPE_S3,
-	}
-	return datadog.LogsArchiveCreateRequest{
-		Data: &datadog.LogsArchiveCreateRequestDefinition{
-			Attributes: &datadog.LogsArchiveCreateRequestAttributes{
-				Destination: datadog.LogsArchiveDestinationS3AsLogsArchiveCreateRequestDestination(&destinationS3),
-				Name:        archiveName,
-				Query:       archiveQuery,
-			},
-			Type: "archives",
-		},
-	}
-}
-
-type logsArchiveTestClient interface {
-	getAll() (datadog.LogsArchives, *_nethttp.Response, error)
-	create(archive datadog.LogsArchiveCreateRequest) (datadog.LogsArchive, *_nethttp.Response, error)
-	getById(archiveId string) (datadog.LogsArchive, *_nethttp.Response, error)
-	deleteById(archiveId string) (*_nethttp.Response, error)
-	updateById(archiveId string, archive datadog.LogsArchiveCreateRequest) (datadog.LogsArchive, *_nethttp.Response, error)
-}
-
-type apiClientTest struct {
-	ctx    context.Context
-	client *datadog.APIClient
-}
-
-func (r *apiClientTest) getAll() (datadog.LogsArchives, *_nethttp.Response, error) {
-	return r.client.LogsArchivesApi.ListLogsArchives(r.ctx).Execute()
-}
-
-func (r *apiClientTest) create(archive datadog.LogsArchiveCreateRequest) (datadog.LogsArchive, *_nethttp.Response, error) {
-	return r.client.LogsArchivesApi.CreateLogsArchive(r.ctx).Body(archive).Execute()
-}
-
-func (r *apiClientTest) updateById(archiveId string, archive datadog.LogsArchiveCreateRequest) (datadog.LogsArchive, *_nethttp.Response, error) {
-	return r.client.LogsArchivesApi.UpdateLogsArchive(r.ctx, archiveId).Body(archive).Execute()
-}
-
-func (r *apiClientTest) getById(archiveId string) (datadog.LogsArchive, *_nethttp.Response, error) {
-	return r.client.LogsArchivesApi.GetLogsArchive(r.ctx, archiveId).Execute()
-}
-
-func (r *apiClientTest) deleteById(archiveId string) (*_nethttp.Response, error) {
-	return r.client.LogsArchivesApi.DeleteLogsArchive(r.ctx, archiveId).Execute()
-}
-
-// Test Lifecycle
-
-// 1. create(archive) -> (archive with id, httresp with code 200, nil)
-// 2. updateById(archiveId, archive with Query = "source") -> (archive, httresp with code 200, nil)
-// 3. getById(archiveId) -> (archive with id, httresp with code 200, nil)
-// 4. getAll() -> ([archive with id], httresp with code 200, nil)
-
-type apiDummyClient struct {
-	archives map[string]datadog.LogsArchive
-	client   apiClientTest
-	URL      string
-}
-
-func (r *apiDummyClient) getAll() (datadog.LogsArchives, *_nethttp.Response, error) {
-	archiveDefs := make([]datadog.LogsArchiveDefinition, len(r.archives))
-	for _, archive := range r.archives {
-		archiveDefs = append(archiveDefs, *archive.Data)
-	}
-	result := datadog.NewLogsArchives()
-	result.Data = &archiveDefs
-	gock.New(r.URL).Get("api/v2/logs/config/archives").Reply(200).JSON(result)
-	defer gock.Off()
-	return r.client.getAll()
-}
-
-func (r *apiDummyClient) create(archive datadog.LogsArchiveCreateRequest) (datadog.LogsArchive, *_nethttp.Response, error) {
-	id := RandString()
-	newArchive := logsArchiveCreateRequestToLogArchive(id, archive)
-	r.archives[id] = newArchive
-	gock.New(r.URL).Post("/api/v2/logs/config/archives").MatchType("json").JSON(archive).Reply(200).JSON(newArchive)
-	defer gock.Off()
-	return r.client.create(archive)
-}
-
-func (r *apiDummyClient) updateById(archiveId string, archive datadog.LogsArchiveCreateRequest) (datadog.LogsArchive, *_nethttp.Response, error) {
-	if _, exists := r.archives[archiveId]; exists {
-		newArchive := logsArchiveCreateRequestToLogArchive(archiveId, archive)
-		r.archives[archiveId] = newArchive
-		gock.New(r.URL).Put(fmt.Sprintf("api/v2/logs/config/archives/%s", archiveId)).MatchType("json").JSON(archive).Reply(200).JSON(newArchive)
-	} else {
-		newArchive := datadog.NewLogsArchive()
-		gock.New(r.URL).Put(fmt.Sprintf("api/v2/logs/config/archives/%s", archiveId)).MatchType("json").JSON(archive).Reply(404).JSON(newArchive)
-	}
-	defer gock.Off()
-	return r.client.updateById(archiveId, archive)
-}
-
-func (r *apiDummyClient) getById(archiveId string) (datadog.LogsArchive, *_nethttp.Response, error) {
-	if value, exists := r.archives[archiveId]; exists {
-		gock.New(r.URL).Get("api/v2/logs/config/archives").Reply(200).JSON(value)
-	} else {
-		gock.New(r.URL).Get("api/v2/logs/config/archives").Reply(404).JSON(datadog.NewLogsArchive())
-	}
-	defer gock.Off()
-	return r.client.getById(archiveId)
-}
-
-func (r *apiDummyClient) deleteById(archiveId string) (*_nethttp.Response, error) {
-	if _, exists := r.archives[archiveId]; exists {
-		delete(r.archives, archiveId)
-		gock.New(r.URL).Delete("api/v2/logs/config/archives").Reply(204)
-	} else {
-		gock.New(r.URL).Delete("api/v2/logs/config/archives").Reply(404)
-	}
-	return r.client.deleteById(archiveId)
-}
-
-func logsArchiveCreateRequestToLogArchive(id string, archive datadog.LogsArchiveCreateRequest) datadog.LogsArchive {
-	return datadog.LogsArchive{
-		Data: &datadog.LogsArchiveDefinition{
-			Attributes: &datadog.LogsArchiveAttributes{
-				Destination: *datadog.NewNullableLogsArchiveDestination(&datadog.LogsArchiveDestination{
-					LogsArchiveDestinationS3:    archive.Data.Attributes.Destination.LogsArchiveDestinationS3,
-					LogsArchiveDestinationAzure: archive.Data.Attributes.Destination.LogsArchiveDestinationAzure,
-					LogsArchiveDestinationGCS:   archive.Data.Attributes.Destination.LogsArchiveDestinationGCS,
-				}),
-				Name:  archive.Data.Attributes.Name,
-				Query: archive.Data.Attributes.Query,
-			},
-			Type: archive.Data.Type,
-			Id:   datadog.PtrString(id),
-		},
-	}
-}
-
-func assertNoError(t *testing.T, err error, httpresp *_nethttp.Response) {
+func readFixture(t *testing.T, path string) string {
+	res, err := tests.ReadFixture(path)
 	if err != nil {
-		if openApiError, ok := err.(datadog.GenericOpenAPIError); ok {
-			t.Fatalf("Error creating logs archive: Response: %s, Http Resp: %v", openApiError.Body(), httpresp)
-		} else {
-			t.Fatalf("Error creating logs archive: Response: %s, Http Resp: %v", err, httpresp)
-		}
+		t.Fatalf("Failed to read fixture: %s", err)
 	}
-}
-
-const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-func RandString() string {
-	n := 10
-	b := make([]byte, n)
-	for i := range b {
-		b[i] = letterBytes[rand.Intn(len(letterBytes))]
-	}
-	return string(b)
+	return res
 }
