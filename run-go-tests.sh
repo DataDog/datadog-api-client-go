6f3b2b0fda7074ab5aacc1f56651377301d81671
Jiri Kuncar <jiri.kuncar@gmail.com>
2020-07-20 12:53:01 +0200

    Bash arrays

--- run-go-tests.sh
+++ run-go-tests.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+CMD=( go test -coverpkg=$(go list ./... | grep -v /test | paste -sd "," -) -coverprofile=coverage.txt -covermode=atomic $(go list ./...) -json )
+
 if [ "$#" -ne 2 ]; then
-	go test -coverpkg=$(go list ./... | grep -v /test | paste -sd "," -) -coverprofile=coverage.txt -covermode=atomic $(go list ./...) -json
+	"${CMD[@]}"
 else
 	if [ "$RERECORD_FAILED_TESTS" == "true" ]; then
-		RECORD=true go test -coverpkg=$(go list ./... | grep -v /test | paste -sd "," -) -coverprofile=coverage.txt -covermode=atomic -json $(go list ./...) $1 $2
+		RECORD=true "${CMD[@]}" $1 $2
 	else
-		go test -coverpkg=$(go list ./... | grep -v /test | paste -sd "," -) -coverprofile=coverage.txt -covermode=atomic -json $(go list ./...) $1 $2
+		"${CMD[@]}" $1 $2
 	fi
 fi
