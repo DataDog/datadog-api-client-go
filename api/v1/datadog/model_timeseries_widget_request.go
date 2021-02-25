125bf1afc467422428751dffb332617302ad9169
ci.datadog-api-spec <packages@datadoghq.com>
2021-02-25 21:17:41 +0000

    Regenerate client from commit 5016587 of spec repo

--- api/v1/datadog/model_timeseries_widget_request.go
+++ api/v1/datadog/model_timeseries_widget_request.go
@@ -17,7 +17,7 @@ type TimeseriesWidgetRequest struct {
 	ApmQuery    *LogQueryDefinition `json:"apm_query,omitempty"`
 	DisplayType *WidgetDisplayType  `json:"display_type,omitempty"`
 	EventQuery  *LogQueryDefinition `json:"event_query,omitempty"`
-	// List of formulas that operate on queries. This feature is currently in beta.
+	// List of formulas that operate on queries. **This feature is currently in beta.**
 	Formulas *[]WidgetFormula    `json:"formulas,omitempty"`
 	LogQuery *LogQueryDefinition `json:"log_query,omitempty"`
 	// Used to define expression aliases.
@@ -29,7 +29,7 @@ type TimeseriesWidgetRequest struct {
 	ProfileMetricsQuery *LogQueryDefinition     `json:"profile_metrics_query,omitempty"`
 	// Widget query.
 	Q *string `json:"q,omitempty"`
-	// List of queries that can be returned directly or used in formulas. This feature is currently in beta.
+	// List of queries that can be returned directly or used in formulas. **This feature is currently in beta.**
 	Queries        *[]FormulaAndFunctionQueryDefinition `json:"queries,omitempty"`
 	ResponseFormat *FormulaAndFunctionResponseFormat    `json:"response_format,omitempty"`
 	RumQuery       *LogQueryDefinition                  `json:"rum_query,omitempty"`
