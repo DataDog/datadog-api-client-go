22ddc47a713581d9142911b4324054d13102f55a
ci.datadog-api-spec <packages@datadoghq.com>
2021-04-28 10:12:29 +0000

    Regenerate client from commit 75ca6ae of spec repo

--- api/v1/datadog/model_image_widget_definition.go
+++ api/v1/datadog/model_image_widget_definition.go
@@ -14,11 +14,19 @@ import (
 
 // ImageWidgetDefinition The image widget allows you to embed an image on your dashboard. An image can be a PNG, JPG, or animated GIF. Only available on FREE layout dashboards.
 type ImageWidgetDefinition struct {
-	Margin *WidgetMargin             `json:"margin,omitempty"`
-	Sizing *WidgetImageSizing        `json:"sizing,omitempty"`
-	Type   ImageWidgetDefinitionType `json:"type"`
+	// Whether to display a background or not.
+	HasBackground *bool `json:"has_background,omitempty"`
+	// Whether to display a border or not.
+	HasBorder       *bool                     `json:"has_border,omitempty"`
+	HorizontalAlign *WidgetHorizontalAlign    `json:"horizontal_align,omitempty"`
+	Margin          *WidgetMargin             `json:"margin,omitempty"`
+	Sizing          *WidgetImageSizing        `json:"sizing,omitempty"`
+	Type            ImageWidgetDefinitionType `json:"type"`
 	// URL of the image.
 	Url string `json:"url"`
+	// URL of the image in dark mode.
+	UrlDarkTheme  *string              `json:"url_dark_theme,omitempty"`
+	VerticalAlign *WidgetVerticalAlign `json:"vertical_align,omitempty"`
 }
 
 // NewImageWidgetDefinition instantiates a new ImageWidgetDefinition object
@@ -27,6 +35,10 @@ type ImageWidgetDefinition struct {
 // will change when the set of required properties is changed
 func NewImageWidgetDefinition(type_ ImageWidgetDefinitionType, url string) *ImageWidgetDefinition {
 	this := ImageWidgetDefinition{}
+	var hasBackground bool = true
+	this.HasBackground = &hasBackground
+	var hasBorder bool = true
+	this.HasBorder = &hasBorder
 	this.Type = type_
 	this.Url = url
 	return &this
@@ -37,9 +49,109 @@ func NewImageWidgetDefinition(type_ ImageWidgetDefinitionType, url string) *Imag
 // but it doesn't guarantee that properties required by API are set
 func NewImageWidgetDefinitionWithDefaults() *ImageWidgetDefinition {
 	this := ImageWidgetDefinition{}
+	var hasBackground bool = true
+	this.HasBackground = &hasBackground
+	var hasBorder bool = true
+	this.HasBorder = &hasBorder
 	var type_ ImageWidgetDefinitionType = IMAGEWIDGETDEFINITIONTYPE_IMAGE
 	this.Type = type_
 	return &this
+}
+
+// GetHasBackground returns the HasBackground field value if set, zero value otherwise.
+func (o *ImageWidgetDefinition) GetHasBackground() bool {
+	if o == nil || o.HasBackground == nil {
+		var ret bool
+		return ret
+	}
+	return *o.HasBackground
+}
+
+// GetHasBackgroundOk returns a tuple with the HasBackground field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *ImageWidgetDefinition) GetHasBackgroundOk() (*bool, bool) {
+	if o == nil || o.HasBackground == nil {
+		return nil, false
+	}
+	return o.HasBackground, true
+}
+
+// HasHasBackground returns a boolean if a field has been set.
+func (o *ImageWidgetDefinition) HasHasBackground() bool {
+	if o != nil && o.HasBackground != nil {
+		return true
+	}
+
+	return false
+}
+
+// SetHasBackground gets a reference to the given bool and assigns it to the HasBackground field.
+func (o *ImageWidgetDefinition) SetHasBackground(v bool) {
+	o.HasBackground = &v
+}
+
+// GetHasBorder returns the HasBorder field value if set, zero value otherwise.
+func (o *ImageWidgetDefinition) GetHasBorder() bool {
+	if o == nil || o.HasBorder == nil {
+		var ret bool
+		return ret
+	}
+	return *o.HasBorder
+}
+
+// GetHasBorderOk returns a tuple with the HasBorder field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *ImageWidgetDefinition) GetHasBorderOk() (*bool, bool) {
+	if o == nil || o.HasBorder == nil {
+		return nil, false
+	}
+	return o.HasBorder, true
+}
+
+// HasHasBorder returns a boolean if a field has been set.
+func (o *ImageWidgetDefinition) HasHasBorder() bool {
+	if o != nil && o.HasBorder != nil {
+		return true
+	}
+
+	return false
+}
+
+// SetHasBorder gets a reference to the given bool and assigns it to the HasBorder field.
+func (o *ImageWidgetDefinition) SetHasBorder(v bool) {
+	o.HasBorder = &v
+}
+
+// GetHorizontalAlign returns the HorizontalAlign field value if set, zero value otherwise.
+func (o *ImageWidgetDefinition) GetHorizontalAlign() WidgetHorizontalAlign {
+	if o == nil || o.HorizontalAlign == nil {
+		var ret WidgetHorizontalAlign
+		return ret
+	}
+	return *o.HorizontalAlign
+}
+
+// GetHorizontalAlignOk returns a tuple with the HorizontalAlign field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *ImageWidgetDefinition) GetHorizontalAlignOk() (*WidgetHorizontalAlign, bool) {
+	if o == nil || o.HorizontalAlign == nil {
+		return nil, false
+	}
+	return o.HorizontalAlign, true
+}
+
+// HasHorizontalAlign returns a boolean if a field has been set.
+func (o *ImageWidgetDefinition) HasHorizontalAlign() bool {
+	if o != nil && o.HorizontalAlign != nil {
+		return true
+	}
+
+	return false
+}
+
+// SetHorizontalAlign gets a reference to the given WidgetHorizontalAlign and assigns it to the HorizontalAlign field.
+func (o *ImageWidgetDefinition) SetHorizontalAlign(v WidgetHorizontalAlign) {
+	o.HorizontalAlign = &v
 }
 
 // GetMargin returns the Margin field value if set, zero value otherwise.
@@ -154,8 +266,81 @@ func (o *ImageWidgetDefinition) SetUrl(v string) {
 	o.Url = v
 }
 
+// GetUrlDarkTheme returns the UrlDarkTheme field value if set, zero value otherwise.
+func (o *ImageWidgetDefinition) GetUrlDarkTheme() string {
+	if o == nil || o.UrlDarkTheme == nil {
+		var ret string
+		return ret
+	}
+	return *o.UrlDarkTheme
+}
+
+// GetUrlDarkThemeOk returns a tuple with the UrlDarkTheme field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *ImageWidgetDefinition) GetUrlDarkThemeOk() (*string, bool) {
+	if o == nil || o.UrlDarkTheme == nil {
+		return nil, false
+	}
+	return o.UrlDarkTheme, true
+}
+
+// HasUrlDarkTheme returns a boolean if a field has been set.
+func (o *ImageWidgetDefinition) HasUrlDarkTheme() bool {
+	if o != nil && o.UrlDarkTheme != nil {
+		return true
+	}
+
+	return false
+}
+
+// SetUrlDarkTheme gets a reference to the given string and assigns it to the UrlDarkTheme field.
+func (o *ImageWidgetDefinition) SetUrlDarkTheme(v string) {
+	o.UrlDarkTheme = &v
+}
+
+// GetVerticalAlign returns the VerticalAlign field value if set, zero value otherwise.
+func (o *ImageWidgetDefinition) GetVerticalAlign() WidgetVerticalAlign {
+	if o == nil || o.VerticalAlign == nil {
+		var ret WidgetVerticalAlign
+		return ret
+	}
+	return *o.VerticalAlign
+}
+
+// GetVerticalAlignOk returns a tuple with the VerticalAlign field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *ImageWidgetDefinition) GetVerticalAlignOk() (*WidgetVerticalAlign, bool) {
+	if o == nil || o.VerticalAlign == nil {
+		return nil, false
+	}
+	return o.VerticalAlign, true
+}
+
+// HasVerticalAlign returns a boolean if a field has been set.
+func (o *ImageWidgetDefinition) HasVerticalAlign() bool {
+	if o != nil && o.VerticalAlign != nil {
+		return true
+	}
+
+	return false
+}
+
+// SetVerticalAlign gets a reference to the given WidgetVerticalAlign and assigns it to the VerticalAlign field.
+func (o *ImageWidgetDefinition) SetVerticalAlign(v WidgetVerticalAlign) {
+	o.VerticalAlign = &v
+}
+
 func (o ImageWidgetDefinition) MarshalJSON() ([]byte, error) {
 	toSerialize := map[string]interface{}{}
+	if o.HasBackground != nil {
+		toSerialize["has_background"] = o.HasBackground
+	}
+	if o.HasBorder != nil {
+		toSerialize["has_border"] = o.HasBorder
+	}
+	if o.HorizontalAlign != nil {
+		toSerialize["horizontal_align"] = o.HorizontalAlign
+	}
 	if o.Margin != nil {
 		toSerialize["margin"] = o.Margin
 	}
@@ -167,6 +352,12 @@ func (o ImageWidgetDefinition) MarshalJSON() ([]byte, error) {
 	}
 	if true {
 		toSerialize["url"] = o.Url
+	}
+	if o.UrlDarkTheme != nil {
+		toSerialize["url_dark_theme"] = o.UrlDarkTheme
+	}
+	if o.VerticalAlign != nil {
+		toSerialize["vertical_align"] = o.VerticalAlign
 	}
 	return json.Marshal(toSerialize)
 }
