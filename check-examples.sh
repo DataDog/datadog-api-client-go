cb6953f1b306e7472ae6820ef5d911b6053b9139
Thomas Hervé <thomas.herve@datadoghq.com>
2021-05-03 16:36:59 +0200

    Fix check examples

--- check-examples.sh
+++ check-examples.sh
@@ -2,13 +2,13 @@
 
 ./extract-code-blocks.sh
 
-for f in examples/v*/*/*.go ; do
+for f in examples/v*/*.go ; do
     df=$(dirname $f)/$(basename $f .go)
     mkdir -p $df
     mv $f $df/main.go
 done
 
-ls examples/v*/*/*/main.go | xargs -P $(($(nproc)*2)) -n 1 go build -o /dev/null
+ls examples/v*/*/main.go | xargs -P $(($(nproc)*2)) -n 1 go build -o /dev/null
 if [ $? -ne 0 ]; then
     echo -e "Failed to build examples"
     exit 1
