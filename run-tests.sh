5189ec6fe1c07c945757b3b6096a767aacdd84bd
Noueman Khalikine <noueman.khal@gmail.com>
2021-05-12 22:39:26 +0200

    Enables run-tests to output the error message

--- run-tests.sh
+++ run-tests.sh
@@ -6,10 +6,12 @@ go mod tidy
 ALL_DEPS=`cat go.sum | awk '{print $1}' | uniq | sort | sed "s|^\(.*\)|go.sum,\1,|"`
 DEPS_NOT_FOUND=""
 for one_dep in `echo $ALL_DEPS`; do
+    set +e
     cat LICENSE-3rdparty.csv | grep "$one_dep" > /dev/null 2>&1
     if [ $? -ne 0 ]; then
         DEPS_NOT_FOUND="${DEPS_NOT_FOUND}\n${one_dep}<LICENSE>,<COPYRIGHT>"
     fi
+    set -e
 done
 if [ -n "$DEPS_NOT_FOUND" ]; then
     echo "Some dependencies were not found in LICENSE-3rdparty.csv, please add: $DEPS_NOT_FOUND"
