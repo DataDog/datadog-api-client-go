df07d341d4de4dac9d7dbe71dcdb9b2c3850b783
Hippolyte HENRY <hippolyte.henry@datadoghq.com>
2021-06-02 09:10:18 +0200

    Remove comments, details

--- .github/workflows/scripts/test_integration.js
+++ .github/workflows/scripts/test_integration.js
@@ -1,8 +1,10 @@
+const SPEC_REPO = "datadog-api-spec"
+
 module.exports.post_status_check = async (github, context, status) => {
     const pr_num = context.payload.pull_request.head.ref.split("/")[2]
     const {data: pr} = await github.pulls.get({
         owner: context.repo.owner,
-        repo: "datadog-api-spec",
+        repo: SPEC_REPO,
         pull_number: pr_num,
     });
     const { data: jobs } = await github.actions.listJobsForWorkflowRun({
@@ -12,12 +14,11 @@ module.exports.post_status_check = async (github, context, status) => {
     });
     await github.repos.createCommitStatus({
         owner: context.repo.owner,
-        repo: "datadog-api-spec",
+        repo: SPEC_REPO,
         sha: pr.head.sha,
         state: status,
-        target_url: `https://github.com/DataDog/datadog-api-client-go/pull/973/checks?check_run_id=${jobs.jobs[0].id}`,
+        target_url: `https://github.com/${context.repo.owner}/${context.repo.repo}/pull/${context.payload.pull_request.number}/checks?check_run_id=${jobs.jobs[0].id}`,
         description: `${context.repo.repo} integration tests`,
         context: `${context.repo.repo}_integration_tests`
     });
-
 }
