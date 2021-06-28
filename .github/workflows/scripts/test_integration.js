3cbce15af54b88cfdec3b42353558514e320f831
Jiri Kuncar <jiri.kuncar@gmail.com>
2021-06-28 13:19:15 +0200

    ci: report status for unit tests

--- .github/workflows/scripts/test_integration.js
+++ .github/workflows/scripts/test_integration.js
@@ -1,6 +1,6 @@
 const SPEC_REPO = "datadog-api-spec"
 
-module.exports.post_status_check = async (github, context, status) => {
+module.exports.post_status_check = async (github, context, test, status) => {
     const pr_num = context.payload.pull_request.head.ref.split("/")[2]
     const {data: pr} = await github.pulls.get({
         owner: context.repo.owner,
@@ -18,7 +18,7 @@ module.exports.post_status_check = async (github, context, status) => {
         sha: pr.head.sha,
         state: status,
         target_url: `https://github.com/${context.repo.owner}/${context.repo.repo}/pull/${context.payload.pull_request.number}/checks?check_run_id=${jobs.jobs[0].id}`,
-        description: `${context.repo.repo} integration tests`,
-        context: `${context.repo.repo}_integration_tests`
+        description: `${context.repo.repo} ${test} tests`,
+        context: `${context.repo.repo}_${test}_tests`
     });
 }
