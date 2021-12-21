12b403e581e08c9276b6b9a9bb877d20ccc8d247
ci.datadog-api-spec <packages@datadoghq.com>
2021-12-21 22:02:53 +0000

    Regenerate client from commit d1a4fc3 of spec repo

---
+++ examples/v1/downtimes/CreateDowntime_1393233946.go
@@ -0,0 +1,49 @@
+// Schedule a downtime with until occurrences
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+
+	datadog "github.com/DataDog/datadog-api-client-go/api/v1/datadog"
+)
+
+func main() {
+	body := datadog.Downtime{
+		Message: datadog.PtrString("Example-Schedule_a_downtime_with_until_occurrences"),
+		Recurrence: *datadog.NewNullableDowntimeRecurrence(&datadog.DowntimeRecurrence{
+			Period:           datadog.PtrInt32(1),
+			Type:             datadog.PtrString("weeks"),
+			UntilOccurrences: *datadog.NewNullableInt32(datadog.PtrInt32(3)),
+			WeekDays: &[]string{
+				"Mon",
+				"Tue",
+				"Wed",
+				"Thu",
+				"Fri",
+			},
+		}),
+		Scope: &[]string{
+			"*",
+		},
+		Start:    datadog.PtrInt64(time.Now().Unix()),
+		End:      *datadog.NewNullableInt64(datadog.PtrInt64(time.Now().Add(time.Hour * 1).Unix())),
+		Timezone: datadog.PtrString("Etc/UTC"),
+	}
+	ctx := datadog.NewDefaultContext(context.Background())
+	configuration := datadog.NewConfiguration()
+	apiClient := datadog.NewAPIClient(configuration)
+	resp, r, err := apiClient.DowntimesApi.CreateDowntime(ctx, body)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `DowntimesApi.CreateDowntime`: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+	}
+
+	responseContent, _ := json.MarshalIndent(resp, "", "  ")
+	fmt.Fprintf(os.Stdout, "Response from `DowntimesApi.CreateDowntime`:\n%s\n", responseContent)
+}
---
+++ examples/v2/key-management/ListApplicationKeys_2237010090.go
@@ -0,0 +1,27 @@
+// Get all Application keys returns "OK" response
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	datadog "github.com/DataDog/datadog-api-client-go/api/v2/datadog"
+)
+
+func main() {
+	ctx := datadog.NewDefaultContext(context.Background())
+	configuration := datadog.NewConfiguration()
+	apiClient := datadog.NewAPIClient(configuration)
+	resp, r, err := apiClient.KeyManagementApi.ListApplicationKeys(ctx, *datadog.NewListApplicationKeysOptionalParameters())
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `KeyManagementApi.ListApplicationKeys`: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+	}
+
+	responseContent, _ := json.MarshalIndent(resp, "", "  ")
+	fmt.Fprintf(os.Stdout, "Response from `KeyManagementApi.ListApplicationKeys`:\n%s\n", responseContent)
+}
