c906cb7bd781fcce283da3457bae03e0a8270577
ci.datadog-api-spec <packages@datadoghq.com>
2022-01-18 12:06:19 +0000

    Regenerate client from commit 9646439 of spec repo

--- api/v2/datadog/docs/SecurityMonitoringRuleDetectionMethod.md
+++ api/v2/datadog/docs/SecurityMonitoringRuleDetectionMethod.md
@@ -8,4 +8,6 @@
 
 - `ANOMALY_DETECTION` (value: `"anomaly_detection"`)
 
+- `NO_DATA` (value: `"no_data"`)
+
 [[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
--- api/v2/datadog/model_security_monitoring_rule_detection_method.go
+++ api/v2/datadog/model_security_monitoring_rule_detection_method.go
@@ -21,12 +21,14 @@ const (
 	SECURITYMONITORINGRULEDETECTIONMETHOD_THRESHOLD         SecurityMonitoringRuleDetectionMethod = "threshold"
 	SECURITYMONITORINGRULEDETECTIONMETHOD_NEW_VALUE         SecurityMonitoringRuleDetectionMethod = "new_value"
 	SECURITYMONITORINGRULEDETECTIONMETHOD_ANOMALY_DETECTION SecurityMonitoringRuleDetectionMethod = "anomaly_detection"
+	SECURITYMONITORINGRULEDETECTIONMETHOD_NO_DATA           SecurityMonitoringRuleDetectionMethod = "no_data"
 )
 
 var allowedSecurityMonitoringRuleDetectionMethodEnumValues = []SecurityMonitoringRuleDetectionMethod{
 	"threshold",
 	"new_value",
 	"anomaly_detection",
+	"no_data",
 }
 
 func (w *SecurityMonitoringRuleDetectionMethod) GetAllowedValues() []SecurityMonitoringRuleDetectionMethod {
