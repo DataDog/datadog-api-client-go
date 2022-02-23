e2e0fdaf214c461a883b6af57782b974e6b88563
ci.datadog-api-spec <packages@datadoghq.com>
2022-02-23 10:11:03 +0000

    Regenerate client from commit cf974e8 of spec repo

---
+++ examples/v1/synthetics/CreateSyntheticsAPITest_1487281163.go
@@ -0,0 +1,112 @@
+// Create an API HTTP test returns "OK - Returns the created test details." response
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	datadog "github.com/DataDog/datadog-api-client-go/api/v1/datadog"
+)
+
+func main() {
+	body := datadog.SyntheticsAPITest{
+		Config: datadog.SyntheticsAPITestConfig{
+			Assertions: &[]datadog.SyntheticsAssertion{
+				datadog.SyntheticsAssertion{
+					SyntheticsAssertionTarget: &datadog.SyntheticsAssertionTarget{
+						Operator: datadog.SYNTHETICSASSERTIONOPERATOR_IS,
+						Property: datadog.PtrString("{{ PROPERTY }}"),
+						Target:   "text/html",
+						Type:     datadog.SYNTHETICSASSERTIONTYPE_HEADER,
+					}},
+				datadog.SyntheticsAssertion{
+					SyntheticsAssertionTarget: &datadog.SyntheticsAssertionTarget{
+						Operator: datadog.SYNTHETICSASSERTIONOPERATOR_LESS_THAN,
+						Target:   2000,
+						Type:     datadog.SYNTHETICSASSERTIONTYPE_RESPONSE_TIME,
+					}},
+				datadog.SyntheticsAssertion{
+					SyntheticsAssertionJSONPathTarget: &datadog.SyntheticsAssertionJSONPathTarget{
+						Operator: datadog.SYNTHETICSASSERTIONJSONPATHOPERATOR_VALIDATES_JSON_PATH,
+						Target: &datadog.SyntheticsAssertionJSONPathTargetTarget{
+							JsonPath:    datadog.PtrString("topKey"),
+							Operator:    datadog.PtrString("isNot"),
+							TargetValue: "0",
+						},
+						Type: datadog.SYNTHETICSASSERTIONTYPE_BODY,
+					}},
+			},
+			ConfigVariables: &[]datadog.SyntheticsConfigVariable{
+				{
+					Example: datadog.PtrString("content-type"),
+					Name:    "PROPERTY",
+					Pattern: datadog.PtrString("content-type"),
+					Type:    datadog.SYNTHETICSCONFIGVARIABLETYPE_TEXT,
+				},
+			},
+			Request: &datadog.SyntheticsTestRequest{
+				Certificate: &datadog.SyntheticsTestRequestCertificate{
+					Cert: &datadog.SyntheticsTestRequestCertificateItem{
+						Content:   datadog.PtrString("cert-content"),
+						Filename:  datadog.PtrString("cert-filename"),
+						UpdatedAt: datadog.PtrString("2020-10-16T09:23:24.857Z"),
+					},
+					Key: &datadog.SyntheticsTestRequestCertificateItem{
+						Content:   datadog.PtrString("key-content"),
+						Filename:  datadog.PtrString("key-filename"),
+						UpdatedAt: datadog.PtrString("2020-10-16T09:23:24.857Z"),
+					},
+				},
+				Headers: &map[string]string{
+					"unique": "examplecreateanapihttptestreturnsokreturnsthecreatedtestdetailsresponse",
+				},
+				Method:  datadog.HTTPMETHOD_GET.Ptr(),
+				Timeout: datadog.PtrFloat64(10),
+				Url:     datadog.PtrString("https://datadoghq.com"),
+				Proxy: &datadog.SyntheticsTestRequestProxy{
+					Url:     "https://datadoghq.com",
+					Headers: &map[string]string{},
+				},
+			},
+		},
+		Locations: []string{
+			"aws:us-east-2",
+		},
+		Message: datadog.PtrString("BDD test payload: synthetics_api_http_test_payload.json"),
+		Name:    "Example-Create_an_API_HTTP_test_returns_OK_Returns_the_created_test_details_response",
+		Options: datadog.SyntheticsTestOptions{
+			AcceptSelfSigned:   datadog.PtrBool(false),
+			AllowInsecure:      datadog.PtrBool(true),
+			FollowRedirects:    datadog.PtrBool(true),
+			MinFailureDuration: datadog.PtrInt64(10),
+			MinLocationFailed:  datadog.PtrInt64(1),
+			MonitorName:        datadog.PtrString("Example-Create_an_API_HTTP_test_returns_OK_Returns_the_created_test_details_response"),
+			MonitorPriority:    datadog.PtrInt32(5),
+			Retry: &datadog.SyntheticsTestOptionsRetry{
+				Count:    datadog.PtrInt64(3),
+				Interval: datadog.PtrFloat64(10),
+			},
+			TickEvery: datadog.PtrInt64(60),
+		},
+		Subtype: datadog.SYNTHETICSTESTDETAILSSUBTYPE_HTTP.Ptr(),
+		Tags: &[]string{
+			"testing:api",
+		},
+		Type: datadog.SYNTHETICSAPITESTTYPE_API,
+	}
+	ctx := datadog.NewDefaultContext(context.Background())
+	configuration := datadog.NewConfiguration()
+	apiClient := datadog.NewAPIClient(configuration)
+	resp, r, err := apiClient.SyntheticsApi.CreateSyntheticsAPITest(ctx, body)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `SyntheticsApi.CreateSyntheticsAPITest`: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+	}
+
+	responseContent, _ := json.MarshalIndent(resp, "", "  ")
+	fmt.Fprintf(os.Stdout, "Response from `SyntheticsApi.CreateSyntheticsAPITest`:\n%s\n", responseContent)
+}
