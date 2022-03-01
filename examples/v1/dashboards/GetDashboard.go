0b5f033d7d9869546dd959db9931373bf83a530b
ci.datadog-api-spec <packages@datadoghq.com>
2022-03-01 10:12:52 +0000

    Regenerate client from commit 7fe3e9a of spec repo

---
+++ examples/v1/dashboards/GetDashboard.go
@@ -0,0 +1,30 @@
+// Get a dashboard returns "OK" response
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	datadog "github.com/DataDog/datadog-api-client-go/api/v1/datadog"
+)
+
+func main() {
+	// there is a valid "dashboard" in the system
+	DashboardID := os.Getenv("DASHBOARD_ID")
+
+	ctx := datadog.NewDefaultContext(context.Background())
+	configuration := datadog.NewConfiguration()
+	apiClient := datadog.NewAPIClient(configuration)
+	resp, r, err := apiClient.DashboardsApi.GetDashboard(ctx, DashboardID)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `DashboardsApi.GetDashboard`: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+	}
+
+	responseContent, _ := json.MarshalIndent(resp, "", "  ")
+	fmt.Fprintf(os.Stdout, "Response from `DashboardsApi.GetDashboard`:\n%s\n", responseContent)
+}
