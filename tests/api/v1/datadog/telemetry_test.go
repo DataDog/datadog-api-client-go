e70f648b01d55ecaa0164a281ebb83321cff8349
skarimo <40482491+skarimo@users.noreply.github.com>
2022-03-03 12:43:28 -0500

    Fix telemetry test regex (#1366)
    * fix telemetry regex
    
    * Apply code review suggestions
    
    * Apply code review suggestions
    
    Co-authored-by: Jiri Kuncar <jiri.kuncar@datadoghq.com>
    
    Co-authored-by: Jiri Kuncar <jiri.kuncar@datadoghq.com>

--- tests/api/v1/datadog/telemetry_test.go
+++ tests/api/v1/datadog/telemetry_test.go
@@ -19,7 +19,7 @@ func TestTelemetryHeaders(t *testing.T) {
 	assert.NoError(err)
 	gock.New(URL).
 		Get("integration/aws").
-		MatchHeader("User-Agent", "^datadog-api-client-go/\\d\\.\\d\\.\\d.*? \\(go .*?; os .*?; arch .*?\\)$").
+		MatchHeader("User-Agent", "^datadog-api-client-go\\/\\d+\\.\\d+\\.\\d+.*? \\(go .*?; os .*?; arch .*?\\)$").
 		Reply(299)
 	defer gock.Off()
 
