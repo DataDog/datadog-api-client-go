b65c8c405cfb7a12a8e7f35170f3313f8d83855d
ci.datadog-api-spec <packages@datadoghq.com>
2022-03-03 15:07:26 +0000

    Regenerate client from commit cb0a2c0 of spec repo

--- examples/v1/synthetics/GetAPITestResult_1321866518.go
+++ examples/v1/synthetics/GetAPITestResult_1321866518.go
@@ -13,15 +13,15 @@ import (
 
 func main() {
 	// there is a "synthetics_api_test_with_wrong_dns" in the system
-	SyntheticsAPITestWithWrongDnsPublicID := os.Getenv("SYNTHETICS_API_TEST_WITH_WRONG_DNS_PUBLIC_ID")
+	SyntheticsAPITestWithWrongDNSPublicID := os.Getenv("SYNTHETICS_API_TEST_WITH_WRONG_DNS_PUBLIC_ID")
 
 	// the "synthetics_api_test_with_wrong_dns" is triggered
-	SyntheticsAPITestWithWrongDnsResultResults0ResultID := os.Getenv("SYNTHETICS_API_TEST_WITH_WRONG_DNS_RESULT_RESULTS_0_RESULT_ID")
+	SyntheticsAPITestWithWrongDNSResultResults0ResultID := os.Getenv("SYNTHETICS_API_TEST_WITH_WRONG_DNS_RESULT_RESULTS_0_RESULT_ID")
 
 	ctx := datadog.NewDefaultContext(context.Background())
 	configuration := datadog.NewConfiguration()
 	apiClient := datadog.NewAPIClient(configuration)
-	resp, r, err := apiClient.SyntheticsApi.GetAPITestResult(ctx, SyntheticsAPITestWithWrongDnsPublicID, SyntheticsAPITestWithWrongDnsResultResults0ResultID)
+	resp, r, err := apiClient.SyntheticsApi.GetAPITestResult(ctx, SyntheticsAPITestWithWrongDNSPublicID, SyntheticsAPITestWithWrongDNSResultResults0ResultID)
 
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error when calling `SyntheticsApi.GetAPITestResult`: %v\n", err)
