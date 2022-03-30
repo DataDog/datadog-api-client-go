9ff56876a84772f89b3c85625af0e705439928dd
ci.datadog-api-spec <packages@datadoghq.com>
2022-03-30 20:57:55 +0000

    Regenerate client from commit 442fcd8b of spec repo

--- examples/v2/dashboard-lists/CreateDashboardListItems_3995409989.go
+++ examples/v2/dashboard-lists/CreateDashboardListItems_3995409989.go
@@ -1,4 +1,4 @@
-// Update items of a dashboard list returns "OK" response
+// Add custom timeboard dashboard to an existing dashboard list returns "OK" response
 
 package main
 
@@ -16,27 +16,27 @@ func main() {
 	// there is a valid "dashboard_list" in the system
 	DashboardListID, _ := strconv.ParseInt(os.Getenv("DASHBOARD_LIST_ID"), 10, 64)
 
-	// there is a valid "screenboard_dashboard" in the system
-	ScreenboardDashboardID := os.Getenv("SCREENBOARD_DASHBOARD_ID")
+	// there is a valid "dashboard" in the system
+	DashboardID := os.Getenv("DASHBOARD_ID")
 
-	body := datadog.DashboardListUpdateItemsRequest{
+	body := datadog.DashboardListAddItemsRequest{
 		Dashboards: &[]datadog.DashboardListItemRequest{
 			{
-				Id:   ScreenboardDashboardID,
-				Type: datadog.DASHBOARDTYPE_CUSTOM_SCREENBOARD,
+				Id:   DashboardID,
+				Type: datadog.DASHBOARDTYPE_CUSTOM_TIMEBOARD,
 			},
 		},
 	}
 	ctx := datadog.NewDefaultContext(context.Background())
 	configuration := datadog.NewConfiguration()
 	apiClient := datadog.NewAPIClient(configuration)
-	resp, r, err := apiClient.DashboardListsApi.UpdateDashboardListItems(ctx, DashboardListID, body)
+	resp, r, err := apiClient.DashboardListsApi.CreateDashboardListItems(ctx, DashboardListID, body)
 
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error when calling `DashboardListsApi.UpdateDashboardListItems`: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Error when calling `DashboardListsApi.CreateDashboardListItems`: %v\n", err)
 		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
 	}
 
 	responseContent, _ := json.MarshalIndent(resp, "", "  ")
-	fmt.Fprintf(os.Stdout, "Response from `DashboardListsApi.UpdateDashboardListItems`:\n%s\n", responseContent)
+	fmt.Fprintf(os.Stdout, "Response from `DashboardListsApi.CreateDashboardListItems`:\n%s\n", responseContent)
 }
