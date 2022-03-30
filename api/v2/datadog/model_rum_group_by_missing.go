38307d8be1db6c188e58907c65105e5b0a83196b
ci.datadog-api-spec <packages@datadoghq.com>
2022-03-30 15:02:27 +0000

    Regenerate client from commit 51301706 of spec repo

---
+++ api/v2/datadog/model_rum_group_by_missing.go
@@ -0,0 +1,150 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-Present Datadog, Inc.
+ */
+
+package datadog
+
+import (
+	"encoding/json"
+)
+
+// RUMGroupByMissing - The value to use for logs that don't have the facet used to group by.
+type RUMGroupByMissing struct {
+	RUMGroupByMissingString *string
+	RUMGroupByMissingNumber *float64
+
+	// UnparsedObject contains the raw value of the object if there was an error when deserializing into the struct
+	UnparsedObject interface{}
+}
+
+// RUMGroupByMissingStringAsRUMGroupByMissing is a convenience function that returns string wrapped in RUMGroupByMissing
+func RUMGroupByMissingStringAsRUMGroupByMissing(v *string) RUMGroupByMissing {
+	return RUMGroupByMissing{RUMGroupByMissingString: v}
+}
+
+// RUMGroupByMissingNumberAsRUMGroupByMissing is a convenience function that returns float64 wrapped in RUMGroupByMissing
+func RUMGroupByMissingNumberAsRUMGroupByMissing(v *float64) RUMGroupByMissing {
+	return RUMGroupByMissing{RUMGroupByMissingNumber: v}
+}
+
+// Unmarshal JSON data into one of the pointers in the struct
+func (dst *RUMGroupByMissing) UnmarshalJSON(data []byte) error {
+	var err error
+	match := 0
+	// try to unmarshal data into RUMGroupByMissingString
+	err = json.Unmarshal(data, &dst.RUMGroupByMissingString)
+	if err == nil {
+		if dst.RUMGroupByMissingString != nil {
+			jsonRUMGroupByMissingString, _ := json.Marshal(dst.RUMGroupByMissingString)
+			if string(jsonRUMGroupByMissingString) == "{}" { // empty struct
+				dst.RUMGroupByMissingString = nil
+			} else {
+				match++
+			}
+		} else {
+			dst.RUMGroupByMissingString = nil
+		}
+	} else {
+		dst.RUMGroupByMissingString = nil
+	}
+
+	// try to unmarshal data into RUMGroupByMissingNumber
+	err = json.Unmarshal(data, &dst.RUMGroupByMissingNumber)
+	if err == nil {
+		if dst.RUMGroupByMissingNumber != nil {
+			jsonRUMGroupByMissingNumber, _ := json.Marshal(dst.RUMGroupByMissingNumber)
+			if string(jsonRUMGroupByMissingNumber) == "{}" { // empty struct
+				dst.RUMGroupByMissingNumber = nil
+			} else {
+				match++
+			}
+		} else {
+			dst.RUMGroupByMissingNumber = nil
+		}
+	} else {
+		dst.RUMGroupByMissingNumber = nil
+	}
+
+	if match != 1 { // more than 1 match
+		// reset to nil
+		dst.RUMGroupByMissingString = nil
+		dst.RUMGroupByMissingNumber = nil
+		return json.Unmarshal(data, &dst.UnparsedObject)
+	} else {
+		return nil // exactly one match
+	}
+}
+
+// Marshal data from the first non-nil pointers in the struct to JSON
+func (src RUMGroupByMissing) MarshalJSON() ([]byte, error) {
+	if src.RUMGroupByMissingString != nil {
+		return json.Marshal(&src.RUMGroupByMissingString)
+	}
+
+	if src.RUMGroupByMissingNumber != nil {
+		return json.Marshal(&src.RUMGroupByMissingNumber)
+	}
+
+	if src.UnparsedObject != nil {
+		return json.Marshal(src.UnparsedObject)
+	}
+	return nil, nil // no data in oneOf schemas
+}
+
+// Get the actual instance
+func (obj *RUMGroupByMissing) GetActualInstance() interface{} {
+	if obj.RUMGroupByMissingString != nil {
+		return obj.RUMGroupByMissingString
+	}
+
+	if obj.RUMGroupByMissingNumber != nil {
+		return obj.RUMGroupByMissingNumber
+	}
+
+	// all schemas are nil
+	return nil
+}
+
+type NullableRUMGroupByMissing struct {
+	value *RUMGroupByMissing
+	isSet bool
+}
+
+func (v NullableRUMGroupByMissing) Get() *RUMGroupByMissing {
+	return v.value
+}
+
+func (v *NullableRUMGroupByMissing) Set(val *RUMGroupByMissing) {
+	v.value = val
+	v.isSet = true
+}
+
+func (v NullableRUMGroupByMissing) IsSet() bool {
+	return v.isSet
+}
+
+func (v *NullableRUMGroupByMissing) Unset() {
+	v.value = nil
+	v.isSet = false
+}
+
+func NewNullableRUMGroupByMissing(val *RUMGroupByMissing) *NullableRUMGroupByMissing {
+	return &NullableRUMGroupByMissing{value: val, isSet: true}
+}
+
+func (v NullableRUMGroupByMissing) MarshalJSON() ([]byte, error) {
+	return json.Marshal(v.value)
+}
+
+func (v *NullableRUMGroupByMissing) UnmarshalJSON(src []byte) error {
+	v.isSet = true
+
+	// this object is nullable so check if the payload is null or empty string
+	if string(src) == "" || string(src) == "{}" {
+		return nil
+	}
+
+	return json.Unmarshal(src, &v.value)
+}
---
+++ api/v2/datadog/model_rum_sort_order.go
@@ -0,0 +1,102 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-Present Datadog, Inc.
+ */
+
+package datadog
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// RUMSortOrder The order to use, ascending or descending.
+type RUMSortOrder string
+
+// List of RUMSortOrder
+const (
+	RUMSORTORDER_ASCENDING  RUMSortOrder = "asc"
+	RUMSORTORDER_DESCENDING RUMSortOrder = "desc"
+)
+
+var allowedRUMSortOrderEnumValues = []RUMSortOrder{
+	RUMSORTORDER_ASCENDING,
+	RUMSORTORDER_DESCENDING,
+}
+
+func (w *RUMSortOrder) GetAllowedValues() []RUMSortOrder {
+	return allowedRUMSortOrderEnumValues
+}
+
+func (v *RUMSortOrder) UnmarshalJSON(src []byte) error {
+	var value string
+	err := json.Unmarshal(src, &value)
+	if err != nil {
+		return err
+	}
+	*v = RUMSortOrder(value)
+	return nil
+}
+
+// NewRUMSortOrderFromValue returns a pointer to a valid RUMSortOrder
+// for the value passed as argument, or an error if the value passed is not allowed by the enum
+func NewRUMSortOrderFromValue(v string) (*RUMSortOrder, error) {
+	ev := RUMSortOrder(v)
+	if ev.IsValid() {
+		return &ev, nil
+	} else {
+		return nil, fmt.Errorf("invalid value '%v' for RUMSortOrder: valid values are %v", v, allowedRUMSortOrderEnumValues)
+	}
+}
+
+// IsValid return true if the value is valid for the enum, false otherwise
+func (v RUMSortOrder) IsValid() bool {
+	for _, existing := range allowedRUMSortOrderEnumValues {
+		if existing == v {
+			return true
+		}
+	}
+	return false
+}
+
+// Ptr returns reference to RUMSortOrder value
+func (v RUMSortOrder) Ptr() *RUMSortOrder {
+	return &v
+}
+
+type NullableRUMSortOrder struct {
+	value *RUMSortOrder
+	isSet bool
+}
+
+func (v NullableRUMSortOrder) Get() *RUMSortOrder {
+	return v.value
+}
+
+func (v *NullableRUMSortOrder) Set(val *RUMSortOrder) {
+	v.value = val
+	v.isSet = true
+}
+
+func (v NullableRUMSortOrder) IsSet() bool {
+	return v.isSet
+}
+
+func (v *NullableRUMSortOrder) Unset() {
+	v.value = nil
+	v.isSet = false
+}
+
+func NewNullableRUMSortOrder(val *RUMSortOrder) *NullableRUMSortOrder {
+	return &NullableRUMSortOrder{value: val, isSet: true}
+}
+
+func (v NullableRUMSortOrder) MarshalJSON() ([]byte, error) {
+	return json.Marshal(v.value)
+}
+
+func (v *NullableRUMSortOrder) UnmarshalJSON(src []byte) error {
+	v.isSet = true
+	return json.Unmarshal(src, &v.value)
+}
