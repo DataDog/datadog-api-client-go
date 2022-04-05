6edee576748fbdb5de2038c7c1a0618195fa964b
Jiri Kuncar <jiri.kuncar@datadoghq.com>
2022-04-05 16:16:16 +0200

    Update check-examples.sh
    Co-authored-by: Thomas Hervé <thomas.herve@datadoghq.com>

--- check-examples.sh
+++ check-examples.sh
@@ -8,7 +8,7 @@ for f in examples/*/*/*.go ; do
     cp "$f" "$df/main.go"
 done
 
-if (find ./build/examples/*/*/* -type d -print0 | xargs -0 -P8 go build -o /dev/null -ldflags "-s -w"); then
+if (find ./build/examples/*/*/* -type d -print0 | xargs -0 go build -o /dev/null -ldflags "-s -w"); then
     echo -e "Examples are buildable"
 else
     echo -e "Failed to build examples"
