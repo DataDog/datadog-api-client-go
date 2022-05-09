17ed86be48209b496c789bbe24d03d918a913ea9
Thomas Hervé <thomas.herve@datadoghq.com>
2022-05-09 15:54:59 +0200

    Fix example commit
    gitignore wasn't updated so we didn't push examples anymore when we
    moved generation.

---
+++ examples/v2/usage-metering/GetUsageApplicationSecurityMonitoring.go
@@ -0,0 +1,28 @@
+// Get hourly usage for Application Security returns "OK" response
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+
+	datadog "github.com/DataDog/datadog-api-client-go/api/v2/datadog"
+)
+
+func main() {
+	ctx := datadog.NewDefaultContext(context.Background())
+	configuration := datadog.NewConfiguration()
+	apiClient := datadog.NewAPIClient(configuration)
+	resp, r, err := apiClient.UsageMeteringApi.GetUsageApplicationSecurityMonitoring(ctx, time.Now().AddDate(0, 0, -5), *datadog.NewGetUsageApplicationSecurityMonitoringOptionalParameters().WithEndHr(time.Now().AddDate(0, 0, -3)))
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `UsageMeteringApi.GetUsageApplicationSecurityMonitoring`: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+	}
+
+	responseContent, _ := json.MarshalIndent(resp, "", "  ")
+	fmt.Fprintf(os.Stdout, "Response from `UsageMeteringApi.GetUsageApplicationSecurityMonitoring`:\n%s\n", responseContent)
+}
