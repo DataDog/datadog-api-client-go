8f3bed81cd3c58d71c5c4940f9dd0162a481d438
ci.datadog-api-spec <packages@datadoghq.com>
2022-06-10 18:24:20 +0000

    Regenerate client from commit 4569968f of spec repo

---
+++ examples/v1/metrics/SubmitDistributionPoints.go
@@ -0,0 +1,44 @@
+// Submit distribution points returns "Payload accepted" response
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+
+	datadog "github.com/DataDog/datadog-api-client-go/api/v1/datadog"
+)
+
+func main() {
+	body := datadog.DistributionPointsPayload{
+		Series: []datadog.DistributionPointsSeries{
+			{
+				Metric: "system.load.1.dist",
+				Points: [][]datadog.DistributionPointItem{
+					{
+						{DistributionPointTimestamp: datadog.PtrFloat64(float64(time.Now().Unix()))},
+						{DistributionPointData: &[]float64{
+							1.0,
+							2.0,
+						}},
+					},
+				},
+			},
+		},
+	}
+	ctx := datadog.NewDefaultContext(context.Background())
+	configuration := datadog.NewConfiguration()
+	apiClient := datadog.NewAPIClient(configuration)
+	resp, r, err := apiClient.MetricsApi.SubmitDistributionPoints(ctx, body, *datadog.NewSubmitDistributionPointsOptionalParameters())
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `MetricsApi.SubmitDistributionPoints`: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+	}
+
+	responseContent, _ := json.MarshalIndent(resp, "", "  ")
+	fmt.Fprintf(os.Stdout, "Response from `MetricsApi.SubmitDistributionPoints`:\n%s\n", responseContent)
+}
