db54d4a480e11da2568c36ac35e1adc2bba23c4d
Thomas Hervé <thomas.herve@datadoghq.com>
2022-07-26 19:42:23 +0200

    Generate pagination examples (#1609)
    * Generate pagination examples
    
    * Fix response usage

---
+++ examples/v2/logs/ListLogsGet_738202670.go
@@ -0,0 +1,30 @@
+// Get a list of logs returns "OK" response with pagination
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/DataDog/datadog-api-client-go/api/common"
+	datadog "github.com/DataDog/datadog-api-client-go/api/v2/datadog"
+)
+
+func main() {
+	ctx := common.NewDefaultContext(context.Background())
+	configuration := common.NewConfiguration()
+	apiClient := common.NewAPIClient(configuration)
+	api := datadog.NewLogsApi(apiClient)
+	resp, _, err := api.ListLogsGetWithPagination(ctx, *datadog.NewListLogsGetOptionalParameters().WithPageLimit(2))
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `LogsApi.ListLogsGet`: %v\n", err)
+	}
+
+	for item := range resp {
+		responseContent, _ := json.MarshalIndent(item, "", "  ")
+		fmt.Fprintf(os.Stdout, "%s\n", responseContent)
+	}
+}
---
+++ examples/v2/security-monitoring/ListSecurityMonitoringSignals_3960412991.go
@@ -0,0 +1,30 @@
+// Get a quick list of security signals returns "OK" response with pagination
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/DataDog/datadog-api-client-go/api/common"
+	datadog "github.com/DataDog/datadog-api-client-go/api/v2/datadog"
+)
+
+func main() {
+	ctx := common.NewDefaultContext(context.Background())
+	configuration := common.NewConfiguration()
+	apiClient := common.NewAPIClient(configuration)
+	api := datadog.NewSecurityMonitoringApi(apiClient)
+	resp, _, err := api.ListSecurityMonitoringSignalsWithPagination(ctx, *datadog.NewListSecurityMonitoringSignalsOptionalParameters().WithPageLimit(2))
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `SecurityMonitoringApi.ListSecurityMonitoringSignals`: %v\n", err)
+	}
+
+	for item := range resp {
+		responseContent, _ := json.MarshalIndent(item, "", "  ")
+		fmt.Fprintf(os.Stdout, "%s\n", responseContent)
+	}
+}
