b23f23bc57cefa5ec9b2501e6d5df8b1c47a84da
Sherzod Karimov <sherzod.karimov@datadoghq.com>
2022-09-19 14:37:48 -0400

    use staticcheck

--- run-tests.sh
+++ run-tests.sh
@@ -24,11 +24,11 @@ fi
 # unfortunately there's no better way to fix this than change directory
 # this might get solved in Go 1.14: https://github.com/golang/go/issues/30515
 cd "$(mktemp -d)"
-GO111MODULE=on go install golang.org/x/lint/golint@latest
+GO111MODULE=on go install honnef.co/go/tools/cmd/staticcheck@latest
 GO111MODULE=on go install gotest.tools/gotestsum@latest
 cd -
 
-golint ./...
+staticcheck ./api/...
 go mod tidy
 go clean -testcache
 
