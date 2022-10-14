963d1a9cc0d1d0cc5d068f44a94120cb987279c6
ci.datadog-api-spec <packages@datadoghq.com>
2022-10-14 07:46:10 +0000

    Regenerate client from commit 13ac1a3a of spec repo

---
+++ examples/v2/confluent-cloud/DeleteConfluentAccount_2919241291.go
@@ -0,0 +1,28 @@
+// Delete resource from Confluent account returns "OK" response
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadog"
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadogV2"
+)
+
+func main() {
+	// there is a valid "confluent_account" in the system
+	ConfluentAccountDataID := os.Getenv("CONFLUENT_ACCOUNT_DATA_ID")
+
+	ctx := datadog.NewDefaultContext(context.Background())
+	configuration := datadog.NewConfiguration()
+	apiClient := datadog.NewAPIClient(configuration)
+	api := datadogV2.NewConfluentCloudApi(apiClient)
+	r, err := api.DeleteConfluentAccount(ctx, ConfluentAccountDataID)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `ConfluentCloudApi.DeleteConfluentAccount`: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+	}
+}
