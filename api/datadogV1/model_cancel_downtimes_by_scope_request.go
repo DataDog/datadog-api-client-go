16028539dacf63cfc967d8dd6b3697dfa50bbdc9
ci.datadog-api-spec <packages@datadoghq.com>
2023-03-07 22:37:16 +0000

    Regenerate client from commit 73e66c9a of spec repo

--- api/datadogV1/model_cancel_downtimes_by_scope_request.go
+++ api/datadogV1/model_cancel_downtimes_by_scope_request.go
@@ -11,7 +11,7 @@ import (
 
 // CancelDowntimesByScopeRequest Cancel downtimes according to scope.
 type CancelDowntimesByScopeRequest struct {
-	// The scope(s) to which the downtime applies. For example, `host:app2`.
+	// The scope(s) to which the downtime applies and must be in `key:value` format. For example, `host:app2`.
 	// Provide multiple scopes as a comma-separated list like `env:dev,env:prod`.
 	// The resulting downtime applies to sources that matches ALL provided scopes (`env:dev` **AND** `env:prod`).
 	Scope string `json:"scope"`
