15ce7459c6aefacc4165c79a1d1f005691307daa
skarimo <40482491+skarimo@users.noreply.github.com>
2023-08-02 11:06:07 -0400

    Fix `unparsedObject` deserialization for lists (#2125)
    * fix unparsedObject handling for lists
    
    * apply code review suggestion
    
    * pre-commit fixes
    
    ---------
    
    Co-authored-by: ci.datadog-api-spec <packages@datadoghq.com>

--- api/datadogV2/model_logs_aggregate_bucket_value_timeseries.go
+++ api/datadogV2/model_logs_aggregate_bucket_value_timeseries.go
@@ -47,5 +47,22 @@ func (o LogsAggregateBucketValueTimeseries) MarshalJSON() ([]byte, error) {
 
 // UnmarshalJSON deserializes the given payload.
 func (o *LogsAggregateBucketValueTimeseries) UnmarshalJSON(bytes []byte) (err error) {
-	return json.Unmarshal(bytes, &o.Items)
+	if err = json.Unmarshal(bytes, &o.Items); err != nil {
+		return json.Unmarshal(bytes, &o.UnparsedObject)
+	}
+
+	if o.Items != nil && len(o.Items) > 0 {
+		for _, v := range o.Items {
+			if v.UnparsedObject != nil {
+				err = json.Unmarshal(bytes, &o.UnparsedObject)
+				if err != nil {
+					return err
+				}
+
+				break
+			}
+		}
+	}
+
+	return nil
 }
