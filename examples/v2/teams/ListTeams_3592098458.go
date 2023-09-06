469866d663bc557910db10956ff391d465b560c4
ci.datadog-api-spec <packages@datadoghq.com>
2023-09-06 14:57:22 +0000

    Regenerate client from commit 5896c0ef of spec repo

---
+++ examples/v2/teams/ListTeams_3592098458.go
@@ -0,0 +1,29 @@
+// Get all teams returns "OK" response with pagination
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadog"
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadogV2"
+)
+
+func main() {
+	ctx := datadog.NewDefaultContext(context.Background())
+	configuration := datadog.NewConfiguration()
+	apiClient := datadog.NewAPIClient(configuration)
+	api := datadogV2.NewTeamsApi(apiClient)
+	resp, _ := api.ListTeamsWithPagination(ctx, *datadogV2.NewListTeamsOptionalParameters().WithPageSize(2))
+
+	for paginationResult := range resp {
+		if paginationResult.Error != nil {
+			fmt.Fprintf(os.Stderr, "Error when calling `TeamsApi.ListTeams`: %v\n", paginationResult.Error)
+		}
+		responseContent, _ := json.MarshalIndent(paginationResult.Item, "", "  ")
+		fmt.Fprintf(os.Stdout, "%s\n", responseContent)
+	}
+}
