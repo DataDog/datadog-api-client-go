b133f12289fed73eda8bedcb4940fad83b9110ef
ci.datadog-api-spec <packages@datadoghq.com>
2023-09-13 14:38:25 +0000

    Regenerate client from commit ec46249d of spec repo

---
+++ examples/v1/dashboards/CreateDashboard_3777304439.go
@@ -0,0 +1,75 @@
+// Create a new dashboard with formula and function heatmap widget
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadog"
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadogV1"
+)
+
+func main() {
+	body := datadogV1.Dashboard{
+		Title: "Example-Dashboard",
+		Widgets: []datadogV1.Widget{
+			{
+				Layout: &datadogV1.WidgetLayout{
+					X:      0,
+					Y:      0,
+					Width:  47,
+					Height: 15,
+				},
+				Definition: datadogV1.WidgetDefinition{
+					HeatMapWidgetDefinition: &datadogV1.HeatMapWidgetDefinition{
+						Title:      datadog.PtrString(""),
+						TitleSize:  datadog.PtrString("16"),
+						TitleAlign: datadogV1.WIDGETTEXTALIGN_LEFT.Ptr(),
+						Time:       &datadogV1.WidgetTime{},
+						Type:       datadogV1.HEATMAPWIDGETDEFINITIONTYPE_HEATMAP,
+						Requests: []datadogV1.HeatMapWidgetRequest{
+							{
+								ResponseFormat: datadogV1.FORMULAANDFUNCTIONRESPONSEFORMAT_TIMESERIES.Ptr(),
+								Queries: []datadogV1.FormulaAndFunctionQueryDefinition{
+									datadogV1.FormulaAndFunctionQueryDefinition{
+										FormulaAndFunctionMetricQueryDefinition: &datadogV1.FormulaAndFunctionMetricQueryDefinition{
+											DataSource: datadogV1.FORMULAANDFUNCTIONMETRICDATASOURCE_METRICS,
+											Name:       "query1",
+											Query:      "avg:system.cpu.user{*}",
+										}},
+								},
+								Formulas: []datadogV1.WidgetFormula{
+									{
+										Formula: "query1",
+									},
+								},
+								Style: &datadogV1.WidgetStyle{
+									Palette: datadog.PtrString("dog_classic"),
+								},
+							},
+						},
+					}},
+			},
+		},
+		TemplateVariables: []datadogV1.DashboardTemplateVariable{},
+		LayoutType:        datadogV1.DASHBOARDLAYOUTTYPE_FREE,
+		IsReadOnly:        datadog.PtrBool(false),
+		NotifyList:        *datadog.NewNullableList(&[]string{}),
+	}
+	ctx := datadog.NewDefaultContext(context.Background())
+	configuration := datadog.NewConfiguration()
+	apiClient := datadog.NewAPIClient(configuration)
+	api := datadogV1.NewDashboardsApi(apiClient)
+	resp, r, err := api.CreateDashboard(ctx, body)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `DashboardsApi.CreateDashboard`: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+	}
+
+	responseContent, _ := json.MarshalIndent(resp, "", "  ")
+	fmt.Fprintf(os.Stdout, "Response from `DashboardsApi.CreateDashboard`:\n%s\n", responseContent)
+}
