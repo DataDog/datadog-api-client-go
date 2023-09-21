7acfe21cf8ea7efc6e58019edeb8d7a4a22bf8ca
Kevin Zou <kevin.zou@datadoghq.com>
2023-09-21 14:27:04 -0400

    implement asserting on nested slices

--- tests/lookup.go
+++ tests/lookup.go
@@ -3,12 +3,14 @@ package tests
 import (
 	"errors"
 	"reflect"
+	"regexp"
 	"strconv"
 	"strings"
 )
 
 // ErrNotFound is returned when the key didn't match
 var ErrNotFound = errors.New("unable to find the key")
+var nestedIndexRe = regexp.MustCompile(`\]\[`)
 
 // LookupStringI returns the value found at the given path
 func LookupStringI(i interface{}, path string) (reflect.Value, error) {
@@ -19,19 +21,14 @@ func LookupStringI(i interface{}, path string) (reflect.Value, error) {
 		return value, nil
 	}
 	for _, part := range strings.Split(path, ".") {
-		key, index := parseIndex(part)
+		key, _ := parseIndex(part)
 		value, err = lookupPartI(value, key)
 		if err != nil {
 			break
 		}
-		if index != -1 {
-			if value.Kind() != reflect.Slice || value.Len() <= index {
-				return reflect.Value{}, ErrNotFound
-			}
-			value = value.Index(index)
-			if value.Kind() == reflect.Interface {
-				value = value.Elem()
-			}
+		for _, listIndex := range splitNestedSlices(part) {
+			_, indexValue := parseIndex(listIndex)
+			value = lookupIndexI(value, indexValue)
 		}
 	}
 	return value, err
@@ -54,6 +51,17 @@ func lookupPartI(value reflect.Value, key string) (reflect.Value, error) {
 	return reflect.Value{}, ErrNotFound
 }
 
+func lookupIndexI(value reflect.Value, index int) reflect.Value {
+	if value.Kind() != reflect.Slice || value.Len() <= index || index < 0 {
+		return value
+	}
+	value = value.Index(index)
+	if value.Kind() == reflect.Interface {
+		value = value.Elem()
+	}
+	return value
+}
+
 func parseIndex(part string) (string, int) {
 	opening := strings.Index(part, "[")
 	closing := strings.Index(part, "]")
@@ -65,4 +73,23 @@ func parseIndex(part string) (string, int) {
 		}
 	}
 	return part, -1
+}
+
+// This function splits a nested slice into bracketed indexes
+// Ex, "list[0][1]" is split into ["list[0]", "[1]"]
+// Strings without a nested slice are returned in a slice but unchanged
+// Ex, "list[0]" is returned as ["list[0]"]
+func splitNestedSlices(part string) []string {
+	nestedIndices := nestedIndexRe.FindAllStringIndex(part, -1)
+	if nestedIndices != nil {
+		parts := []string{}
+		lastIdx := 0
+		for _, pair := range nestedIndices {
+			parts = append(parts, part[lastIdx:pair[1]-1])
+			lastIdx = pair[1] - 1
+		}
+		parts = append(parts, part[lastIdx:])
+		return parts
+	}
+	return []string{part}
 }
