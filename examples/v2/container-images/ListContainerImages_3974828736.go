aad992f7f2f273ab1fc4d5f6dd5fc23b96310649
ci.datadog-api-spec <packages@datadoghq.com>
2023-10-12 16:56:10 +0000

    Regenerate client from commit a04c27c5 of spec repo

---
+++ examples/v2/container-images/ListContainerImages_3974828736.go
@@ -0,0 +1,30 @@
+// Get all Container Image groups returns "OK" response
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadog"
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadogV2"
+)
+
+func main() {
+	ctx := datadog.NewDefaultContext(context.Background())
+	configuration := datadog.NewConfiguration()
+	configuration.SetUnstableOperationEnabled("v2.ListContainerImages", true)
+	apiClient := datadog.NewAPIClient(configuration)
+	api := datadogV2.NewContainerImagesApi(apiClient)
+	resp, r, err := api.ListContainerImages(ctx, *datadogV2.NewListContainerImagesOptionalParameters().WithGroupBy("short_image"))
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `ContainerImagesApi.ListContainerImages`: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+	}
+
+	responseContent, _ := json.MarshalIndent(resp, "", "  ")
+	fmt.Fprintf(os.Stdout, "Response from `ContainerImagesApi.ListContainerImages`:\n%s\n", responseContent)
+}
