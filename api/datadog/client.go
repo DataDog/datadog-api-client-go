5063cfcc1111f0d04faac7a71c8a879823540345
Sherzod Karimov <sherzod.karimov@datadoghq.com>
2023-11-20 14:08:33 -0500

    run pre-commit on generated files changes

--- api/datadog/client.go
+++ api/datadog/client.go
@@ -467,7 +467,6 @@ func setBody(body interface{}, contentType string) (bodyBuf *bytes.Buffer, err e
 	if bodyBuf == nil {
 		bodyBuf = &bytes.Buffer{}
 	}
-
 	if reader, ok := body.(io.Reader); ok {
 		_, err = bodyBuf.ReadFrom(reader)
 	} else if fp, ok := body.(**os.File); ok {
