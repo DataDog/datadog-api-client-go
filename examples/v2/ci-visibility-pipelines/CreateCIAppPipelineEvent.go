1938559f943d1d40de9020ff0ad5b6fb851dbf4c
ci.datadog-api-spec <packages@datadoghq.com>
2024-01-03 08:47:20 +0000

    Regenerate client from commit 08a40cb5 of spec repo

--- examples/v2/ci-visibility-pipelines/CreateCIAppPipelineEvent.go
+++ examples/v2/ci-visibility-pipelines/CreateCIAppPipelineEvent.go
@@ -39,7 +39,6 @@ func main() {
 	}
 	ctx := datadog.NewDefaultContext(context.Background())
 	configuration := datadog.NewConfiguration()
-	configuration.SetUnstableOperationEnabled("v2.CreateCIAppPipelineEvent", true)
 	apiClient := datadog.NewAPIClient(configuration)
 	api := datadogV2.NewCIVisibilityPipelinesApi(apiClient)
 	resp, r, err := api.CreateCIAppPipelineEvent(ctx, body)
