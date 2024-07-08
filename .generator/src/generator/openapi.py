17b42ccadef20b94b0e1b5a4f7f3b94152c69f1c
Jack Edmonds <jack.edmonds@datadoghq.com>
2024-07-08 13:13:50 -0400

    Allow extensions in the paths section.

--- .generator/src/generator/openapi.py
+++ .generator/src/generator/openapi.py
@@ -213,6 +213,8 @@ def models(spec):
     name_to_schema = {}
 
     for path in spec["paths"]:
+        if path == "x-merge-override":
+            continue
         for method in spec["paths"][path]:
             operation = spec["paths"][path][method]
 
@@ -236,6 +238,8 @@ def apis(spec):
     operations = {}
 
     for path in spec["paths"]:
+        if path == "x-merge-override":
+            continue
         for method in spec["paths"][path]:
             operation = spec["paths"][path][method]
             tag = operation.get("tags", [None])[0]
