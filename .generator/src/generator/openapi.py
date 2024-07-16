fefcd8081df08cee04a2eafe58e3cb861b068325
Anika Maskara <anika.maskara@datadoghq.com>
2024-07-16 14:30:59 -0400

    updates explicit x-merge-overrides handling to handle generic OpenAPI extensions

--- .generator/src/generator/openapi.py
+++ .generator/src/generator/openapi.py
@@ -213,7 +213,7 @@ def models(spec):
     name_to_schema = {}
 
     for path in spec["paths"]:
-        if path == "x-merge-override":
+        if path.startswith("x-"):
             continue
         for method in spec["paths"][path]:
             operation = spec["paths"][path][method]
@@ -238,7 +238,7 @@ def apis(spec):
     operations = {}
 
     for path in spec["paths"]:
-        if path == "x-merge-override":
+        if path.startswith("x-"):
             continue
         for method in spec["paths"][path]:
             operation = spec["paths"][path][method]
