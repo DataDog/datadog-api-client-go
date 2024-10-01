a187bf40dabe24ae8e307229f26855a1e717c43b
ci.datadog-api-spec <packages@datadoghq.com>
2024-10-01 16:19:11 +0000

    Regenerate client from commit 65dbf403 of spec repo

---
+++ examples/v2/microsoft-teams-integration/GetApiHandle.go
@@ -0,0 +1,29 @@
+// Get handle information returns "OK" response
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadog"
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadogV2"
+)
+
+func main() {
+	ctx := datadog.NewDefaultContext(context.Background())
+	configuration := datadog.NewConfiguration()
+	apiClient := datadog.NewAPIClient(configuration)
+	api := datadogV2.NewMicrosoftTeamsIntegrationApi(apiClient)
+	resp, r, err := api.GetApiHandle(ctx, "handle_id")
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `MicrosoftTeamsIntegrationApi.GetApiHandle`: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+	}
+
+	responseContent, _ := json.MarshalIndent(resp, "", "  ")
+	fmt.Fprintf(os.Stdout, "Response from `MicrosoftTeamsIntegrationApi.GetApiHandle`:\n%s\n", responseContent)
+}
