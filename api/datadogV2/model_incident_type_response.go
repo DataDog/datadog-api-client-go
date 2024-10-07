6f3ca47038bd58ccc3e12e6d530c75d0487f1da1
ci.datadog-api-spec <packages@datadoghq.com>
2024-10-07 14:56:40 +0000

    Regenerate client from commit e9adfd01 of spec repo

---
+++ api/datadogV2/model_incident_type_response.go
@@ -0,0 +1,183 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2019-Present Datadog, Inc.
+
+package datadogV2
+
+import (
+	"fmt"
+
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadog"
+)
+
+// IncidentTypeResponse Incident type response data.
+type IncidentTypeResponse struct {
+	// Incident type's attributes.
+	Attributes *IncidentTypeAttributes `json:"attributes,omitempty"`
+	// The incident type's ID.
+	Id string `json:"id"`
+	// Incident type resource type.
+	Type IncidentTypeType `json:"type"`
+	// UnparsedObject contains the raw value of the object if there was an error when deserializing into the struct
+	UnparsedObject       map[string]interface{} `json:"-"`
+	AdditionalProperties map[string]interface{} `json:"-"`
+}
+
+// NewIncidentTypeResponse instantiates a new IncidentTypeResponse object.
+// This constructor will assign default values to properties that have it defined,
+// and makes sure properties required by API are set, but the set of arguments
+// will change when the set of required properties is changed.
+func NewIncidentTypeResponse(id string, typeVar IncidentTypeType) *IncidentTypeResponse {
+	this := IncidentTypeResponse{}
+	this.Id = id
+	this.Type = typeVar
+	return &this
+}
+
+// NewIncidentTypeResponseWithDefaults instantiates a new IncidentTypeResponse object.
+// This constructor will only assign default values to properties that have it defined,
+// but it doesn't guarantee that properties required by API are set.
+func NewIncidentTypeResponseWithDefaults() *IncidentTypeResponse {
+	this := IncidentTypeResponse{}
+	var typeVar IncidentTypeType = INCIDENTTYPETYPE_INCIDENT_TYPES
+	this.Type = typeVar
+	return &this
+}
+
+// GetAttributes returns the Attributes field value if set, zero value otherwise.
+func (o *IncidentTypeResponse) GetAttributes() IncidentTypeAttributes {
+	if o == nil || o.Attributes == nil {
+		var ret IncidentTypeAttributes
+		return ret
+	}
+	return *o.Attributes
+}
+
+// GetAttributesOk returns a tuple with the Attributes field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *IncidentTypeResponse) GetAttributesOk() (*IncidentTypeAttributes, bool) {
+	if o == nil || o.Attributes == nil {
+		return nil, false
+	}
+	return o.Attributes, true
+}
+
+// HasAttributes returns a boolean if a field has been set.
+func (o *IncidentTypeResponse) HasAttributes() bool {
+	return o != nil && o.Attributes != nil
+}
+
+// SetAttributes gets a reference to the given IncidentTypeAttributes and assigns it to the Attributes field.
+func (o *IncidentTypeResponse) SetAttributes(v IncidentTypeAttributes) {
+	o.Attributes = &v
+}
+
+// GetId returns the Id field value.
+func (o *IncidentTypeResponse) GetId() string {
+	if o == nil {
+		var ret string
+		return ret
+	}
+	return o.Id
+}
+
+// GetIdOk returns a tuple with the Id field value
+// and a boolean to check if the value has been set.
+func (o *IncidentTypeResponse) GetIdOk() (*string, bool) {
+	if o == nil {
+		return nil, false
+	}
+	return &o.Id, true
+}
+
+// SetId sets field value.
+func (o *IncidentTypeResponse) SetId(v string) {
+	o.Id = v
+}
+
+// GetType returns the Type field value.
+func (o *IncidentTypeResponse) GetType() IncidentTypeType {
+	if o == nil {
+		var ret IncidentTypeType
+		return ret
+	}
+	return o.Type
+}
+
+// GetTypeOk returns a tuple with the Type field value
+// and a boolean to check if the value has been set.
+func (o *IncidentTypeResponse) GetTypeOk() (*IncidentTypeType, bool) {
+	if o == nil {
+		return nil, false
+	}
+	return &o.Type, true
+}
+
+// SetType sets field value.
+func (o *IncidentTypeResponse) SetType(v IncidentTypeType) {
+	o.Type = v
+}
+
+// MarshalJSON serializes the struct using spec logic.
+func (o IncidentTypeResponse) MarshalJSON() ([]byte, error) {
+	toSerialize := map[string]interface{}{}
+	if o.UnparsedObject != nil {
+		return datadog.Marshal(o.UnparsedObject)
+	}
+	if o.Attributes != nil {
+		toSerialize["attributes"] = o.Attributes
+	}
+	toSerialize["id"] = o.Id
+	toSerialize["type"] = o.Type
+
+	for key, value := range o.AdditionalProperties {
+		toSerialize[key] = value
+	}
+	return datadog.Marshal(toSerialize)
+}
+
+// UnmarshalJSON deserializes the given payload.
+func (o *IncidentTypeResponse) UnmarshalJSON(bytes []byte) (err error) {
+	all := struct {
+		Attributes *IncidentTypeAttributes `json:"attributes,omitempty"`
+		Id         *string                 `json:"id"`
+		Type       *IncidentTypeType       `json:"type"`
+	}{}
+	if err = datadog.Unmarshal(bytes, &all); err != nil {
+		return datadog.Unmarshal(bytes, &o.UnparsedObject)
+	}
+	if all.Id == nil {
+		return fmt.Errorf("required field id missing")
+	}
+	if all.Type == nil {
+		return fmt.Errorf("required field type missing")
+	}
+	additionalProperties := make(map[string]interface{})
+	if err = datadog.Unmarshal(bytes, &additionalProperties); err == nil {
+		datadog.DeleteKeys(additionalProperties, &[]string{"attributes", "id", "type"})
+	} else {
+		return err
+	}
+
+	hasInvalidField := false
+	if all.Attributes != nil && all.Attributes.UnparsedObject != nil && o.UnparsedObject == nil {
+		hasInvalidField = true
+	}
+	o.Attributes = all.Attributes
+	o.Id = *all.Id
+	if !all.Type.IsValid() {
+		hasInvalidField = true
+	} else {
+		o.Type = *all.Type
+	}
+
+	if len(additionalProperties) > 0 {
+		o.AdditionalProperties = additionalProperties
+	}
+
+	if hasInvalidField {
+		return datadog.Unmarshal(bytes, &o.UnparsedObject)
+	}
+
+	return nil
+}
