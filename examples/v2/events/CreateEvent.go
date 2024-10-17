db89cd128d4b1c9d4faed522a2bd7ed1babaed1d
ci.datadog-api-spec <packages@datadoghq.com>
2024-10-17 16:53:52 +0000

    Regenerate client from commit 3cbe2c1b of spec repo

---
+++ examples/v2/events/CreateEvent.go
@@ -0,0 +1,73 @@
+// Post a change event returns "OK" response
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadog"
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadogV2"
+)
+
+func main() {
+	body := datadogV2.ChangeEventCreateRequestPayload{
+		Data: &datadogV2.ChangeEventCreateRequest{
+			Attributes: &datadogV2.ChangeEvent{
+				Attributes: datadogV2.ChangeEventCustomAttributes{
+					Author: &datadogV2.ChangeEventCustomAttributesAuthor{
+						Name: "datadog@datadog.com",
+						Type: datadogV2.CHANGEEVENTCUSTOMATTRIBUTESAUTHORTYPE_USER,
+					},
+					ChangeMetadata: map[string]interface{}{
+						"dd":            "{'team': 'datadog_team', 'user_email': 'datadog@datadog.com', 'user_id': 'datadog_user_id', 'user_name': 'datadog_username'}",
+						"resource_link": "datadog.com/feature/fallback_payments_test",
+					},
+					ChangedResource: datadogV2.ChangeEventCustomAttributesChangedResource{
+						Name: "fallback_payments_test",
+						Type: datadogV2.CHANGEEVENTCUSTOMATTRIBUTESCHANGEDRESOURCETYPE_FEATURE_FLAG,
+					},
+					ImpactedResources: []datadogV2.ChangeEventCustomAttributesImpactedResourcesItems{
+						{
+							Name: "payments_api",
+							Type: datadogV2.CHANGEEVENTCUSTOMATTRIBUTESIMPACTEDRESOURCESITEMSTYPE_SERVICE,
+						},
+					},
+					NewValue: map[string]interface{}{
+						"enabled":    "True",
+						"percentage": "50%",
+						"rule":       "{'datacenter': 'devcycle.us1.prod'}",
+					},
+					PrevValue: map[string]interface{}{
+						"enabled":    "True",
+						"percentage": "10%",
+						"rule":       "{'datacenter': 'devcycle.us1.prod'}",
+					},
+				},
+				Category: datadogV2.CHANGEEVENTCATEGORY_CHANGE,
+				Message:  datadog.PtrString("payment_processed feature flag has been enabled"),
+				Tags: []string{
+					"environment:test",
+				},
+				Title: "payment_processed feature flag updated",
+			},
+			Type: datadogV2.CHANGEEVENTCREATEREQUESTTYPE_EVENT.Ptr(),
+		},
+	}
+	ctx := datadog.NewDefaultContext(context.Background())
+	configuration := datadog.NewConfiguration()
+	configuration.SetUnstableOperationEnabled("v2.CreateEvent", true)
+	apiClient := datadog.NewAPIClient(configuration)
+	api := datadogV2.NewEventsApi(apiClient)
+	resp, r, err := api.CreateEvent(ctx, body)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `EventsApi.CreateEvent`: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+	}
+
+	responseContent, _ := json.MarshalIndent(resp, "", "  ")
+	fmt.Fprintf(os.Stdout, "Response from `EventsApi.CreateEvent`:\n%s\n", responseContent)
+}
