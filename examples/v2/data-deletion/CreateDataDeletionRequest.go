3c1150344136549d20a9861891c6c28d45428604
ci.datadog-api-spec <packages@datadoghq.com>
2024-12-03 10:18:58 +0000

    Regenerate client from commit 3f8bbb67 of spec repo

---
+++ examples/v2/data-deletion/CreateDataDeletionRequest.go
@@ -0,0 +1,45 @@
+// Creates a data deletion request returns "OK" response
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadog"
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadogV2"
+)
+
+func main() {
+	body := datadogV2.CreateDataDeletionRequestBody{
+		Data: datadogV2.CreateDataDeletionRequestBodyData{
+			Attributes: datadogV2.CreateDataDeletionRequestBodyAttributes{
+				From: 1672527600000,
+				Indexes: []string{
+					"index-1",
+					"index-2",
+				},
+				Query: map[string]string{
+					"host":    "abc",
+					"service": "xyz",
+				},
+				To: 1704063600000,
+			},
+		},
+	}
+	ctx := datadog.NewDefaultContext(context.Background())
+	configuration := datadog.NewConfiguration()
+	apiClient := datadog.NewAPIClient(configuration)
+	api := datadogV2.NewDataDeletionApi(apiClient)
+	resp, r, err := api.CreateDataDeletionRequest(ctx, "logs", body)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `DataDeletionApi.CreateDataDeletionRequest`: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+	}
+
+	responseContent, _ := json.MarshalIndent(resp, "", "  ")
+	fmt.Fprintf(os.Stdout, "Response from `DataDeletionApi.CreateDataDeletionRequest`:\n%s\n", responseContent)
+}
