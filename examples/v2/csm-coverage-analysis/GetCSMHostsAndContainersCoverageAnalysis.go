65bddfb0b258635420b916b9d730976c91e387e8
ci.datadog-api-spec <packages@datadoghq.com>
2024-12-18 15:34:49 +0000

    Regenerate client from commit 4c33d00e of spec repo

---
+++ examples/v2/csm-coverage-analysis/GetCSMHostsAndContainersCoverageAnalysis.go
@@ -0,0 +1,29 @@
+// Get the CSM Hosts and Containers Coverage Analysis returns "OK" response
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadog"
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadogV2"
+)
+
+func main() {
+	ctx := datadog.NewDefaultContext(context.Background())
+	configuration := datadog.NewConfiguration()
+	apiClient := datadog.NewAPIClient(configuration)
+	api := datadogV2.NewCSMCoverageAnalysisApi(apiClient)
+	resp, r, err := api.GetCSMHostsAndContainersCoverageAnalysis(ctx)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `CSMCoverageAnalysisApi.GetCSMHostsAndContainersCoverageAnalysis`: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+	}
+
+	responseContent, _ := json.MarshalIndent(resp, "", "  ")
+	fmt.Fprintf(os.Stdout, "Response from `CSMCoverageAnalysisApi.GetCSMHostsAndContainersCoverageAnalysis`:\n%s\n", responseContent)
+}
