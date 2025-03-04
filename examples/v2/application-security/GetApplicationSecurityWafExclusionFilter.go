23147e8611df319d0e2680689199b04d21b32ae0
ci.datadog-api-spec <packages@datadoghq.com>
2025-03-04 18:21:11 +0000

    Regenerate client from commit fa76ce1e of spec repo

---
+++ examples/v2/application-security/GetApplicationSecurityWafExclusionFilter.go
@@ -0,0 +1,32 @@
+// Get a WAF exclusion filter returns "OK" response
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadog"
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadogV2"
+)
+
+func main() {
+	// there is a valid "exclusion_filter" in the system
+	ExclusionFilterDataID := os.Getenv("EXCLUSION_FILTER_DATA_ID")
+
+	ctx := datadog.NewDefaultContext(context.Background())
+	configuration := datadog.NewConfiguration()
+	apiClient := datadog.NewAPIClient(configuration)
+	api := datadogV2.NewApplicationSecurityApi(apiClient)
+	resp, r, err := api.GetApplicationSecurityWafExclusionFilter(ctx, ExclusionFilterDataID)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `ApplicationSecurityApi.GetApplicationSecurityWafExclusionFilter`: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+	}
+
+	responseContent, _ := json.MarshalIndent(resp, "", "  ")
+	fmt.Fprintf(os.Stdout, "Response from `ApplicationSecurityApi.GetApplicationSecurityWafExclusionFilter`:\n%s\n", responseContent)
+}
