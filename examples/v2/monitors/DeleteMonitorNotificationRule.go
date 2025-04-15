ea0192a5f4648a615abcf077760751ae80eb03e8
ci.datadog-api-spec <packages@datadoghq.com>
2025-04-15 21:56:11 +0000

    Regenerate client from commit 808cf2c3 of spec repo

---
+++ examples/v2/monitors/DeleteMonitorNotificationRule.go
@@ -0,0 +1,29 @@
+// Delete a monitor notification rule returns "OK" response
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadog"
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadogV2"
+)
+
+func main() {
+	// there is a valid "monitor_notification_rule" in the system
+	MonitorNotificationRuleDataID := os.Getenv("MONITOR_NOTIFICATION_RULE_DATA_ID")
+
+	ctx := datadog.NewDefaultContext(context.Background())
+	configuration := datadog.NewConfiguration()
+	configuration.SetUnstableOperationEnabled("v2.DeleteMonitorNotificationRule", true)
+	apiClient := datadog.NewAPIClient(configuration)
+	api := datadogV2.NewMonitorsApi(apiClient)
+	r, err := api.DeleteMonitorNotificationRule(ctx, MonitorNotificationRuleDataID)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `MonitorsApi.DeleteMonitorNotificationRule`: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+	}
+}
