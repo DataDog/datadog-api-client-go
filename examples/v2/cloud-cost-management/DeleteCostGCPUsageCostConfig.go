0077750caa436f5ae4850f7ee83ffa87cc93dd76
ci.datadog-api-spec <packages@datadoghq.com>
2025-06-20 20:08:14 +0000

    Regenerate client from commit 66a5c2fe of spec repo

---
+++ examples/v2/cloud-cost-management/DeleteCostGCPUsageCostConfig.go
@@ -0,0 +1,25 @@
+// Delete Cloud Cost Management GCP Usage Cost config returns "No Content" response
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadog"
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadogV2"
+)
+
+func main() {
+	ctx := datadog.NewDefaultContext(context.Background())
+	configuration := datadog.NewConfiguration()
+	apiClient := datadog.NewAPIClient(configuration)
+	api := datadogV2.NewCloudCostManagementApi(apiClient)
+	r, err := api.DeleteCostGCPUsageCostConfig(ctx, "100")
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `CloudCostManagementApi.DeleteCostGCPUsageCostConfig`: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+	}
+}
