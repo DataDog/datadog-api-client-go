c3cd3002e0d9d09ac86629873a5864993273d7c8
ci.datadog-api-spec <packages@datadoghq.com>
2025-08-08 19:33:41 +0000

    Regenerate client from commit 546fde5 of spec repo

--- api/datadogV1/model_geomap_widget_definition_view.go
+++ api/datadogV1/model_geomap_widget_definition_view.go
@@ -12,7 +12,9 @@ import (
 
 // GeomapWidgetDefinitionView The view of the world that the map should render.
 type GeomapWidgetDefinitionView struct {
-	// The 2-letter ISO code of a country to focus the map on. Or `WORLD`.
+	// A custom extent of the map defined by an array of four numbers in the order `[minLongitude, minLatitude, maxLongitude, maxLatitude]`.
+	CustomExtent []float64 `json:"custom_extent,omitempty"`
+	// The ISO code of a country, sub-division, or region to focus the map on. Or `WORLD`. Mutually exclusive with `custom_extent`.
 	Focus string `json:"focus"`
 	// UnparsedObject contains the raw value of the object if there was an error when deserializing into the struct
 	UnparsedObject       map[string]interface{} `json:"-"`
@@ -35,6 +37,34 @@ func NewGeomapWidgetDefinitionView(focus string) *GeomapWidgetDefinitionView {
 func NewGeomapWidgetDefinitionViewWithDefaults() *GeomapWidgetDefinitionView {
 	this := GeomapWidgetDefinitionView{}
 	return &this
+}
+
+// GetCustomExtent returns the CustomExtent field value if set, zero value otherwise.
+func (o *GeomapWidgetDefinitionView) GetCustomExtent() []float64 {
+	if o == nil || o.CustomExtent == nil {
+		var ret []float64
+		return ret
+	}
+	return o.CustomExtent
+}
+
+// GetCustomExtentOk returns a tuple with the CustomExtent field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *GeomapWidgetDefinitionView) GetCustomExtentOk() (*[]float64, bool) {
+	if o == nil || o.CustomExtent == nil {
+		return nil, false
+	}
+	return &o.CustomExtent, true
+}
+
+// HasCustomExtent returns a boolean if a field has been set.
+func (o *GeomapWidgetDefinitionView) HasCustomExtent() bool {
+	return o != nil && o.CustomExtent != nil
+}
+
+// SetCustomExtent gets a reference to the given []float64 and assigns it to the CustomExtent field.
+func (o *GeomapWidgetDefinitionView) SetCustomExtent(v []float64) {
+	o.CustomExtent = v
 }
 
 // GetFocus returns the Focus field value.
@@ -66,6 +96,9 @@ func (o GeomapWidgetDefinitionView) MarshalJSON() ([]byte, error) {
 	if o.UnparsedObject != nil {
 		return datadog.Marshal(o.UnparsedObject)
 	}
+	if o.CustomExtent != nil {
+		toSerialize["custom_extent"] = o.CustomExtent
+	}
 	toSerialize["focus"] = o.Focus
 
 	for key, value := range o.AdditionalProperties {
@@ -77,7 +110,8 @@ func (o GeomapWidgetDefinitionView) MarshalJSON() ([]byte, error) {
 // UnmarshalJSON deserializes the given payload.
 func (o *GeomapWidgetDefinitionView) UnmarshalJSON(bytes []byte) (err error) {
 	all := struct {
-		Focus *string `json:"focus"`
+		CustomExtent []float64 `json:"custom_extent,omitempty"`
+		Focus        *string   `json:"focus"`
 	}{}
 	if err = datadog.Unmarshal(bytes, &all); err != nil {
 		return datadog.Unmarshal(bytes, &o.UnparsedObject)
@@ -87,10 +121,11 @@ func (o *GeomapWidgetDefinitionView) UnmarshalJSON(bytes []byte) (err error) {
 	}
 	additionalProperties := make(map[string]interface{})
 	if err = datadog.Unmarshal(bytes, &additionalProperties); err == nil {
-		datadog.DeleteKeys(additionalProperties, &[]string{"focus"})
+		datadog.DeleteKeys(additionalProperties, &[]string{"custom_extent", "focus"})
 	} else {
 		return err
 	}
+	o.CustomExtent = all.CustomExtent
 	o.Focus = *all.Focus
 
 	if len(additionalProperties) > 0 {
