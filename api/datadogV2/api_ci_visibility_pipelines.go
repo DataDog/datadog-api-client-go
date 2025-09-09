6e502985a1f9cfb9d95b7dc5ded1817fd5d9a685
ci.datadog-api-spec <packages@datadoghq.com>
2025-09-09 07:22:13 +0000

    Regenerate client from commit 5302b12 of spec repo

--- api/datadogV2/api_ci_visibility_pipelines.go
+++ api/datadogV2/api_ci_visibility_pipelines.go
@@ -100,6 +100,7 @@ func (a *CIVisibilityPipelinesApi) AggregateCIAppPipelineEvents(ctx _context.Con
 // Send your pipeline event to your Datadog platform over HTTP. For details about how pipeline executions are modeled and what execution types we support, see [Pipeline Data Model And Execution Types](https://docs.datadoghq.com/continuous_integration/guides/pipeline_data_model/).
 //
 // Pipeline events can be submitted with a timestamp that is up to 18 hours in the past.
+// The duration between the event start and end times cannot exceed 1 year.
 func (a *CIVisibilityPipelinesApi) CreateCIAppPipelineEvent(ctx _context.Context, body CIAppCreatePipelineEventRequest) (interface{}, *_nethttp.Response, error) {
 	var (
 		localVarHTTPMethod  = _nethttp.MethodPost
