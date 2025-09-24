748f246a631e4419da662ff5a3a261e79e94af52
ci.datadog-api-spec <packages@datadoghq.com>
2025-09-24 17:31:24 +0000

    Regenerate client from commit 4a62c07 of spec repo

---
+++ examples/v2/agentless-scanning/UpdateAzureScanOptions.go
@@ -0,0 +1,31 @@
+// Update azure scan options returns "No Content" response
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadog"
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadogV2"
+)
+
+func main() {
+	body := datadogV2.AzureScanOptionsInputUpdate{
+		Data: &datadogV2.AzureScanOptionsInputUpdateData{
+			Id:   "12345678-90ab-cdef-1234-567890abcdef",
+			Type: datadogV2.AZURESCANOPTIONSINPUTUPDATEDATATYPE_AZURE_SCAN_OPTIONS,
+		},
+	}
+	ctx := datadog.NewDefaultContext(context.Background())
+	configuration := datadog.NewConfiguration()
+	apiClient := datadog.NewAPIClient(configuration)
+	api := datadogV2.NewAgentlessScanningApi(apiClient)
+	r, err := api.UpdateAzureScanOptions(ctx, "12345678-90ab-cdef-1234-567890abcdef", body)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `AgentlessScanningApi.UpdateAzureScanOptions`: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+	}
+}
