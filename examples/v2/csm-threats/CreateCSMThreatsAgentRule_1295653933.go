d35d1a4fafb366e9bf2308e66b3eb330db6352e6
ci.datadog-api-spec <packages@datadoghq.com>
2025-10-02 12:55:50 +0000

    Regenerate client from commit 2c5c83b of spec repo

--- examples/v2/csm-threats/CreateCSMThreatsAgentRule_1295653933.go
+++ examples/v2/csm-threats/CreateCSMThreatsAgentRule_1295653933.go
@@ -29,9 +29,10 @@ func main() {
 				Actions: []datadogV2.CloudWorkloadSecurityAgentRuleAction{
 					{
 						Set: &datadogV2.CloudWorkloadSecurityAgentRuleActionSet{
-							Name:  datadog.PtrString("test_set"),
-							Value: datadog.PtrString("test_value"),
-							Scope: datadog.PtrString("process"),
+							Name:      datadog.PtrString("test_set"),
+							Value:     datadog.PtrString("test_value"),
+							Scope:     datadog.PtrString("process"),
+							Inherited: datadog.PtrBool(true),
 						},
 					},
 					{
