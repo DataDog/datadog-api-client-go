e08bea5b839f33d345cc817a643d593b833093ad
ci.datadog-api-spec <packages@datadoghq.com>
2025-10-23 17:52:24 +0000

    Regenerate client from commit ed00345 of spec repo

--- api/datadog/configuration.go
+++ api/datadog/configuration.go
@@ -698,6 +698,8 @@ func NewConfiguration() *Configuration {
 			"v2.GetSLOReport":                            false,
 			"v2.GetSLOReportJobStatus":                   false,
 			"v2.GetSPARecommendations":                   false,
+			"v2.CreateSCAResolveVulnerableSymbols":       false,
+			"v2.CreateSCAResult":                         false,
 			"v2.AddMemberTeam":                           false,
 			"v2.ListMemberTeams":                         false,
 			"v2.RemoveMemberTeam":                        false,
