14a841f259d9dec4e5edc3df3c1f950737202c7d
ci.datadog-api-spec <packages@datadoghq.com>
2025-10-31 19:08:46 +0000

    Regenerate client from commit a7c729e of spec repo

---
+++ examples/v2/software-catalog/PreviewCatalogEntities.go
@@ -0,0 +1,29 @@
+// Preview catalog entities returns "Accepted" response
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadog"
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadogV2"
+)
+
+func main() {
+	ctx := datadog.NewDefaultContext(context.Background())
+	configuration := datadog.NewConfiguration()
+	apiClient := datadog.NewAPIClient(configuration)
+	api := datadogV2.NewSoftwareCatalogApi(apiClient)
+	resp, r, err := api.PreviewCatalogEntities(ctx)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `SoftwareCatalogApi.PreviewCatalogEntities`: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+	}
+
+	responseContent, _ := json.MarshalIndent(resp, "", "  ")
+	fmt.Fprintf(os.Stdout, "Response from `SoftwareCatalogApi.PreviewCatalogEntities`:\n%s\n", responseContent)
+}
