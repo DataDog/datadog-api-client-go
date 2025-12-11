b6fa80f2adead8ac939c4aa3c7832b4e59b9625d
ci.datadog-api-spec <packages@datadoghq.com>
2025-12-11 21:08:53 +0000

    Regenerate client from commit ba739a2 of spec repo

--- api/datadog/configuration.go
+++ api/datadog/configuration.go
@@ -314,6 +314,7 @@ func NewConfiguration() *Configuration {
 								"us3.datadoghq.com",
 								"us5.datadoghq.com",
 								"ap1.datadoghq.com",
+								"ap2.datadoghq.com",
 								"datadoghq.eu",
 								"ddog-gov.com",
 							},
