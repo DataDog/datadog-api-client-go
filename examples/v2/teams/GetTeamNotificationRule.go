bd785830b843e7000ece508f588718cab71b0438
ci.datadog-api-spec <packages@datadoghq.com>
2025-12-22 16:18:18 +0000

    Regenerate client from commit 01101e0 of spec repo

---
+++ examples/v2/teams/GetTeamNotificationRule.go
@@ -0,0 +1,29 @@
+// Get team notification rule returns "OK" response
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadog"
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadogV2"
+)
+
+func main() {
+	ctx := datadog.NewDefaultContext(context.Background())
+	configuration := datadog.NewConfiguration()
+	apiClient := datadog.NewAPIClient(configuration)
+	api := datadogV2.NewTeamsApi(apiClient)
+	resp, r, err := api.GetTeamNotificationRule(ctx, "rule_id", "team_id")
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `TeamsApi.GetTeamNotificationRule`: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+	}
+
+	responseContent, _ := json.MarshalIndent(resp, "", "  ")
+	fmt.Fprintf(os.Stdout, "Response from `TeamsApi.GetTeamNotificationRule`:\n%s\n", responseContent)
+}
