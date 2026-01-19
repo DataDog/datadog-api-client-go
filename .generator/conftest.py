703baf44a28871d2436c0800df4f2ef8669b1e33
David Tapiador <david.tapiadordeldujo@datadoghq.com>
2026-01-19 09:24:39 +0100

    provide default value for uuid formats

--- .generator/conftest.py
+++ .generator/conftest.py
@@ -375,6 +375,7 @@ def request_parameter(context, operation_id, api_version, operation_specs, name,
                     value = {
                         "string": {
                             "date-time": "2021-11-11T11:11:11.111+00:00",
+                            "uuid": "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
                             None: name,
                         },
                         "integer": {
