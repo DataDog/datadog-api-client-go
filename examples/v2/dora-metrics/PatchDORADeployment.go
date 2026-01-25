877b7ee219650285e7cc8d9d04503a8909b770e8
ci.datadog-api-spec <packages@datadoghq.com>
2026-01-25 12:20:58 +0000

    Regenerate client from commit e1f3e97 of spec repo

---
+++ examples/v2/dora-metrics/PatchDORADeployment.go
@@ -0,0 +1,42 @@
+// Patch a deployment event returns "Accepted" response
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadog"
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadogV2"
+)
+
+func main() {
+	body := datadogV2.DORADeploymentPatchRequest{
+		Data: datadogV2.DORADeploymentPatchRequestData{
+			Attributes: datadogV2.DORADeploymentPatchRequestAttributes{
+				ChangeFailure: datadog.PtrBool(true),
+				Remediation: &datadogV2.DORADeploymentPatchRemediation{
+					Id:   "eG42zNIkVjM",
+					Type: datadogV2.DORADEPLOYMENTPATCHREMEDIATIONTYPE_ROLLBACK,
+				},
+			},
+			Id:   "z_RwVLi7v4Y",
+			Type: datadogV2.DORADEPLOYMENTPATCHREQUESTDATATYPE_DORA_DEPLOYMENT_PATCH_REQUEST,
+		},
+	}
+	ctx := datadog.NewDefaultContext(context.Background())
+	configuration := datadog.NewConfiguration()
+	apiClient := datadog.NewAPIClient(configuration)
+	api := datadogV2.NewDORAMetricsApi(apiClient)
+	resp, r, err := api.PatchDORADeployment(ctx, "deployment_id", body)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `DORAMetricsApi.PatchDORADeployment`: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+	}
+
+	responseContent, _ := json.MarshalIndent(resp, "", "  ")
+	fmt.Fprintf(os.Stdout, "Response from `DORAMetricsApi.PatchDORADeployment`:\n%s\n", responseContent)
+}
