0df12bd84442892ba8d0b6ab6b0771b44aa419cd
ci.datadog-api-spec <packages@datadoghq.com>
2026-03-25 22:57:55 +0000

    Regenerate client from commit 31f93c6 of spec repo

---
+++ examples/v1/dashboards/CreateDashboard_373890042.go
@@ -0,0 +1,64 @@
+// Create a new dashboard with sankey widget and product analytics data source
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadog"
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadogV1"
+)
+
+func main() {
+	body := datadogV1.Dashboard{
+		Title:       "Example-Dashboard",
+		Description: *datadog.NewNullableString(datadog.PtrString("")),
+		Widgets: []datadogV1.Widget{
+			{
+				Layout: &datadogV1.WidgetLayout{
+					X:      0,
+					Y:      0,
+					Width:  47,
+					Height: 15,
+				},
+				Definition: datadogV1.WidgetDefinition{
+					SankeyWidgetDefinition: &datadogV1.SankeyWidgetDefinition{
+						Title:      datadog.PtrString(""),
+						TitleSize:  datadog.PtrString("16"),
+						TitleAlign: datadogV1.WIDGETTEXTALIGN_LEFT.Ptr(),
+						Type:       datadogV1.SANKEYWIDGETDEFINITIONTYPE_SANKEY,
+						Requests: []datadogV1.SankeyWidgetRequest{
+							datadogV1.SankeyWidgetRequest{
+								SankeyRumRequest: &datadogV1.SankeyRumRequest{
+									Query: datadogV1.SankeyRumQuery{
+										DataSource:  datadogV1.SANKEYRUMDATASOURCE_PRODUCT_ANALYTICS,
+										QueryString: "@type:session",
+										Mode:        datadogV1.SANKEYRUMQUERYMODE_SOURCE,
+									},
+									RequestType: datadogV1.SANKEYWIDGETDEFINITIONTYPE_SANKEY,
+								}},
+						},
+					}},
+			},
+		},
+		TemplateVariables: []datadogV1.DashboardTemplateVariable{},
+		LayoutType:        datadogV1.DASHBOARDLAYOUTTYPE_FREE,
+		NotifyList:        *datadog.NewNullableList(&[]string{}),
+	}
+	ctx := datadog.NewDefaultContext(context.Background())
+	configuration := datadog.NewConfiguration()
+	apiClient := datadog.NewAPIClient(configuration)
+	api := datadogV1.NewDashboardsApi(apiClient)
+	resp, r, err := api.CreateDashboard(ctx, body)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `DashboardsApi.CreateDashboard`: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+	}
+
+	responseContent, _ := json.MarshalIndent(resp, "", "  ")
+	fmt.Fprintf(os.Stdout, "Response from `DashboardsApi.CreateDashboard`:\n%s\n", responseContent)
+}
