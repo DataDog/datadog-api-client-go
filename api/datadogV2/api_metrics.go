e38b072ed4551495a330bbbd8322d1bf0f028687
ci.datadog-api-spec <packages@datadoghq.com>
2026-06-02 17:16:46 +0000

    Regenerate client from commit 57fab8d of spec repo

--- api/datadogV2/api_metrics.go
+++ api/datadogV2/api_metrics.go
@@ -1265,9 +1265,9 @@ func (r *ListVolumesByMetricNameOptionalParameters) WithWindowSeconds(windowSeco
 }
 
 // ListVolumesByMetricName List distinct metric volumes by metric name.
-// View hourly average metric volumes for the given metric name over the look back period.
-//
-// Custom metrics generated in-app from other products will return `null` for ingested volumes.
+// View hourly average cardinality for the given metric name over the look back period.
+// For Metric Name Pricing customers, view total point volume for the given metric name
+// over the look back period.
 func (a *MetricsApi) ListVolumesByMetricName(ctx _context.Context, metricName string, o ...ListVolumesByMetricNameOptionalParameters) (MetricVolumesResponse, *_nethttp.Response, error) {
 	var (
 		localVarHTTPMethod  = _nethttp.MethodGet
