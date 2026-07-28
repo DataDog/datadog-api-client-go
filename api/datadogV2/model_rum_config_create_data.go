941dd3feaa6c789c3681a3d0bd1c42d38e7e3d59
ci.datadog-api-spec <packages@datadoghq.com>
2026-07-28 16:07:33 +0000

    Regenerate client from commit 73e3f8c of spec repo

---
+++ api/datadogV2/model_rum_config_create_data.go
@@ -0,0 +1,148 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2019-Present Datadog, Inc.
+
+package datadogV2
+
+import (
+	"fmt"
+
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadog"
+)
+
+// RumConfigCreateData Object describing the RUM configuration to create.
+type RumConfigCreateData struct {
+	// Attributes of the RUM configuration to create.
+	Attributes RumConfigCreateAttributes `json:"attributes"`
+	// The type of the resource. The value should always be `rum_config`.
+	Type RumConfigType `json:"type"`
+	// UnparsedObject contains the raw value of the object if there was an error when deserializing into the struct
+	UnparsedObject       map[string]interface{} `json:"-"`
+	AdditionalProperties map[string]interface{} `json:"-"`
+}
+
+// NewRumConfigCreateData instantiates a new RumConfigCreateData object.
+// This constructor will assign default values to properties that have it defined,
+// and makes sure properties required by API are set, but the set of arguments
+// will change when the set of required properties is changed.
+func NewRumConfigCreateData(attributes RumConfigCreateAttributes, typeVar RumConfigType) *RumConfigCreateData {
+	this := RumConfigCreateData{}
+	this.Attributes = attributes
+	this.Type = typeVar
+	return &this
+}
+
+// NewRumConfigCreateDataWithDefaults instantiates a new RumConfigCreateData object.
+// This constructor will only assign default values to properties that have it defined,
+// but it doesn't guarantee that properties required by API are set.
+func NewRumConfigCreateDataWithDefaults() *RumConfigCreateData {
+	this := RumConfigCreateData{}
+	var typeVar RumConfigType = RUMCONFIGTYPE_RUM_CONFIG
+	this.Type = typeVar
+	return &this
+}
+
+// GetAttributes returns the Attributes field value.
+func (o *RumConfigCreateData) GetAttributes() RumConfigCreateAttributes {
+	if o == nil {
+		var ret RumConfigCreateAttributes
+		return ret
+	}
+	return o.Attributes
+}
+
+// GetAttributesOk returns a tuple with the Attributes field value
+// and a boolean to check if the value has been set.
+func (o *RumConfigCreateData) GetAttributesOk() (*RumConfigCreateAttributes, bool) {
+	if o == nil {
+		return nil, false
+	}
+	return &o.Attributes, true
+}
+
+// SetAttributes sets field value.
+func (o *RumConfigCreateData) SetAttributes(v RumConfigCreateAttributes) {
+	o.Attributes = v
+}
+
+// GetType returns the Type field value.
+func (o *RumConfigCreateData) GetType() RumConfigType {
+	if o == nil {
+		var ret RumConfigType
+		return ret
+	}
+	return o.Type
+}
+
+// GetTypeOk returns a tuple with the Type field value
+// and a boolean to check if the value has been set.
+func (o *RumConfigCreateData) GetTypeOk() (*RumConfigType, bool) {
+	if o == nil {
+		return nil, false
+	}
+	return &o.Type, true
+}
+
+// SetType sets field value.
+func (o *RumConfigCreateData) SetType(v RumConfigType) {
+	o.Type = v
+}
+
+// MarshalJSON serializes the struct using spec logic.
+func (o RumConfigCreateData) MarshalJSON() ([]byte, error) {
+	toSerialize := map[string]interface{}{}
+	if o.UnparsedObject != nil {
+		return datadog.Marshal(o.UnparsedObject)
+	}
+	toSerialize["attributes"] = o.Attributes
+	toSerialize["type"] = o.Type
+
+	for key, value := range o.AdditionalProperties {
+		toSerialize[key] = value
+	}
+	return datadog.Marshal(toSerialize)
+}
+
+// UnmarshalJSON deserializes the given payload.
+func (o *RumConfigCreateData) UnmarshalJSON(bytes []byte) (err error) {
+	all := struct {
+		Attributes *RumConfigCreateAttributes `json:"attributes"`
+		Type       *RumConfigType             `json:"type"`
+	}{}
+	if err = datadog.Unmarshal(bytes, &all); err != nil {
+		return datadog.Unmarshal(bytes, &o.UnparsedObject)
+	}
+	if all.Attributes == nil {
+		return fmt.Errorf("required field attributes missing")
+	}
+	if all.Type == nil {
+		return fmt.Errorf("required field type missing")
+	}
+	additionalProperties := make(map[string]interface{})
+	if err = datadog.UnmarshalUseNumber(bytes, &additionalProperties); err == nil {
+		datadog.DeleteKeys(additionalProperties, &[]string{"attributes", "type"})
+	} else {
+		return err
+	}
+
+	hasInvalidField := false
+	if all.Attributes.UnparsedObject != nil && o.UnparsedObject == nil {
+		hasInvalidField = true
+	}
+	o.Attributes = *all.Attributes
+	if !all.Type.IsValid() {
+		hasInvalidField = true
+	} else {
+		o.Type = *all.Type
+	}
+
+	if len(additionalProperties) > 0 {
+		o.AdditionalProperties = additionalProperties
+	}
+
+	if hasInvalidField {
+		return datadog.Unmarshal(bytes, &o.UnparsedObject)
+	}
+
+	return nil
+}
