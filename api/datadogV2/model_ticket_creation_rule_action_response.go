a5411f8f75ad253c0afbc53ffdc950709b07ca9a
ci.datadog-api-spec <packages@datadoghq.com>
2026-09-10 17:23:16 +0000

    Regenerate client from commit c3f76df of spec repo

--- api/datadogV2/model_ticket_creation_rule_action_response.go
+++ api/datadogV2/model_ticket_creation_rule_action_response.go
@@ -18,7 +18,11 @@ type TicketCreationRuleActionResponse struct {
 	AssigneeId *uuid.UUID `json:"assignee_id,omitempty"`
 	// The reason the rule was automatically disabled by the system due to a ticketing integration error.
 	AutoDisabledReason *string `json:"auto_disabled_reason,omitempty"`
-	// Custom fields of the Jira issue to create. For the list of available fields, see [Jira documentation](https://developer.atlassian.com/cloud/jira/platform/rest/v2/api-group-issues/#api-rest-api-2-issue-createmeta-projectidorkey-issuetypes-issuetypeid-get).
+	// Target-specific fields of the ticket to create.
+	//
+	// For `target: jira`, the custom fields of the Jira issue. For the list of available fields, see [Jira documentation](https://developer.atlassian.com/cloud/jira/platform/rest/v2/api-group-issues/#api-rest-api-2-issue-createmeta-projectidorkey-issuetypes-issuetypeid-get).
+	//
+	// For `target: linear`, the optional keys `linear_project_id` (string, the identifier of the Linear project the issue is created in) and `linear_label_ids` (array of strings, the identifiers of the Linear labels applied to the issue).
 	Fields interface{} `json:"fields,omitempty"`
 	// The maximum number of tickets the rule may create per day. If exceeded, one final ticket will be created, explaining the limit was hit and link back to the responsible rule.
 	MaxTicketsPerDay int64 `json:"max_tickets_per_day"`
