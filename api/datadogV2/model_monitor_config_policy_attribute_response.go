c4c52ba2f64c08b9a4e1bbd78a9e2d12ec255078
ci.datadog-api-spec <packages@datadoghq.com>
2026-09-14 17:21:14 +0000

    Regenerate client from commit 4d446a6 of spec repo

--- api/datadogV2/model_monitor_config_policy_attribute_response.go
+++ api/datadogV2/model_monitor_config_policy_attribute_response.go
@@ -13,6 +13,8 @@ type MonitorConfigPolicyAttributeResponse struct {
 	// Configuration for the policy.
 	Policy *MonitorConfigPolicyPolicy `json:"policy,omitempty"`
 	// The monitor configuration policy type.
+	// `tag` enforces required tags on monitors.
+	// `downtime` sets a maximum downtime duration for the organization.
 	PolicyType *MonitorConfigPolicyType `json:"policy_type,omitempty"`
 	// UnparsedObject contains the raw value of the object if there was an error when deserializing into the struct
 	UnparsedObject       map[string]interface{} `json:"-"`
