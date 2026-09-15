76d4f26e39aac09f925180cbed6c49ba9176cd89
ci.datadog-api-spec <packages@datadoghq.com>
2026-09-15 16:10:27 +0000

    Regenerate client from commit 01df309 of spec repo

--- examples/v2/oci-integration/GetTenancyConfigs.go
+++ examples/v2/oci-integration/GetTenancyConfigs.go
@@ -15,7 +15,6 @@ import (
 func main() {
 	ctx := datadog.NewDefaultContext(context.Background())
 	configuration := datadog.NewConfiguration()
-	configuration.SetUnstableOperationEnabled("v2.GetTenancyConfigs", true)
 	apiClient := datadog.NewAPIClient(configuration)
 	api := datadogV2.NewOCIIntegrationApi(apiClient)
 	resp, r, err := api.GetTenancyConfigs(ctx)
