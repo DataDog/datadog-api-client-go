11e9de4dc036e7b8e96b3af94a1c93b8b568cbd7
ci.datadog-api-spec <packages@datadoghq.com>
2026-09-15 13:20:10 +0000

    Regenerate client from commit 24238c4 of spec repo

--- api/datadogV2/model_workflow_list_item_attributes.go
+++ api/datadogV2/model_workflow_list_item_attributes.go
@@ -23,6 +23,8 @@ type WorkflowListItemAttributes struct {
 	Published *bool `json:"published,omitempty"`
 	// The effective type of identity used to run the workflow.
 	RunAsUserMode *WorkflowRunAsUserMode `json:"runAsUserMode,omitempty"`
+	// Whether the workflow requires sensitive privileges to run. Only the workflow owner can update this field. This allows it to run actions that use [Execution Policies](https://docs.datadoghq.com/actions/private_actions/execution_policies/).
+	SensitivePrivileges *bool `json:"sensitivePrivileges,omitempty"`
 	// A complete Workflow Automation definition, including its triggers, steps, and connections.
 	Spec *Spec `json:"spec,omitempty"`
 	// Tags of the workflow.
@@ -187,6 +189,34 @@ func (o *WorkflowListItemAttributes) SetRunAsUserMode(v WorkflowRunAsUserMode) {
 	o.RunAsUserMode = &v
 }
 
+// GetSensitivePrivileges returns the SensitivePrivileges field value if set, zero value otherwise.
+func (o *WorkflowListItemAttributes) GetSensitivePrivileges() bool {
+	if o == nil || o.SensitivePrivileges == nil {
+		var ret bool
+		return ret
+	}
+	return *o.SensitivePrivileges
+}
+
+// GetSensitivePrivilegesOk returns a tuple with the SensitivePrivileges field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *WorkflowListItemAttributes) GetSensitivePrivilegesOk() (*bool, bool) {
+	if o == nil || o.SensitivePrivileges == nil {
+		return nil, false
+	}
+	return o.SensitivePrivileges, true
+}
+
+// HasSensitivePrivileges returns a boolean if a field has been set.
+func (o *WorkflowListItemAttributes) HasSensitivePrivileges() bool {
+	return o != nil && o.SensitivePrivileges != nil
+}
+
+// SetSensitivePrivileges gets a reference to the given bool and assigns it to the SensitivePrivileges field.
+func (o *WorkflowListItemAttributes) SetSensitivePrivileges(v bool) {
+	o.SensitivePrivileges = &v
+}
+
 // GetSpec returns the Spec field value if set, zero value otherwise.
 func (o *WorkflowListItemAttributes) GetSpec() Spec {
 	if o == nil || o.Spec == nil {
@@ -294,6 +324,9 @@ func (o WorkflowListItemAttributes) MarshalJSON() ([]byte, error) {
 	if o.RunAsUserMode != nil {
 		toSerialize["runAsUserMode"] = o.RunAsUserMode
 	}
+	if o.SensitivePrivileges != nil {
+		toSerialize["sensitivePrivileges"] = o.SensitivePrivileges
+	}
 	if o.Spec != nil {
 		toSerialize["spec"] = o.Spec
 	}
@@ -317,14 +350,15 @@ func (o WorkflowListItemAttributes) MarshalJSON() ([]byte, error) {
 // UnmarshalJSON deserializes the given payload.
 func (o *WorkflowListItemAttributes) UnmarshalJSON(bytes []byte) (err error) {
 	all := struct {
-		CreatedAt     *time.Time             `json:"createdAt,omitempty"`
-		Description   *string                `json:"description,omitempty"`
-		Name          *string                `json:"name"`
-		Published     *bool                  `json:"published,omitempty"`
-		RunAsUserMode *WorkflowRunAsUserMode `json:"runAsUserMode,omitempty"`
-		Spec          *Spec                  `json:"spec,omitempty"`
-		Tags          []string               `json:"tags,omitempty"`
-		UpdatedAt     *time.Time             `json:"updatedAt,omitempty"`
+		CreatedAt           *time.Time             `json:"createdAt,omitempty"`
+		Description         *string                `json:"description,omitempty"`
+		Name                *string                `json:"name"`
+		Published           *bool                  `json:"published,omitempty"`
+		RunAsUserMode       *WorkflowRunAsUserMode `json:"runAsUserMode,omitempty"`
+		SensitivePrivileges *bool                  `json:"sensitivePrivileges,omitempty"`
+		Spec                *Spec                  `json:"spec,omitempty"`
+		Tags                []string               `json:"tags,omitempty"`
+		UpdatedAt           *time.Time             `json:"updatedAt,omitempty"`
 	}{}
 	if err = datadog.Unmarshal(bytes, &all); err != nil {
 		return datadog.Unmarshal(bytes, &o.UnparsedObject)
@@ -334,7 +368,7 @@ func (o *WorkflowListItemAttributes) UnmarshalJSON(bytes []byte) (err error) {
 	}
 	additionalProperties := make(map[string]interface{})
 	if err = datadog.UnmarshalUseNumber(bytes, &additionalProperties); err == nil {
-		datadog.DeleteKeys(additionalProperties, &[]string{"createdAt", "description", "name", "published", "runAsUserMode", "spec", "tags", "updatedAt"})
+		datadog.DeleteKeys(additionalProperties, &[]string{"createdAt", "description", "name", "published", "runAsUserMode", "sensitivePrivileges", "spec", "tags", "updatedAt"})
 	} else {
 		return err
 	}
@@ -349,6 +383,7 @@ func (o *WorkflowListItemAttributes) UnmarshalJSON(bytes []byte) (err error) {
 	} else {
 		o.RunAsUserMode = all.RunAsUserMode
 	}
+	o.SensitivePrivileges = all.SensitivePrivileges
 	if all.Spec != nil && all.Spec.UnparsedObject != nil && o.UnparsedObject == nil {
 		hasInvalidField = true
 	}
