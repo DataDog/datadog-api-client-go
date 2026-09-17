fd413799a34d0225e017ff1a297b0da3dc44a106
David Tapiador <david.tapiadordeldujo@datadoghq.com>
2026-09-17 15:28:32 +0200

    Support request compression test step

--- tests/scenarios/step_definitions.go
+++ tests/scenarios/step_definitions.go
@@ -543,6 +543,10 @@ func expectArrayContainsValue(t gobdd.StepTest, ctx gobdd.Context, responsePath 
 	t.Errorf("could not find value: %v", templatedValue)
 }
 
+// requestUsesCompression is declarative; the generated replay server validates
+// the Content-Encoding header and compressed request body from the request plan.
+func requestUsesCompression(_ gobdd.StepTest, _ gobdd.Context, _ string) {}
+
 // ConfigureSteps on given suite.
 func ConfigureSteps(s *gobdd.Suite) {
 	steps := map[string]interface{}{
@@ -553,6 +557,7 @@ func ConfigureSteps(s *gobdd.Suite) {
 		`new "([^"]+)" request`:                                                newRequest,
 		`request contains "([^"]+)" parameter from "([^"]+)"`:                  addParameterFrom,
 		`request contains "([^"]+)" parameter with value (.+)`:                 addParameterWithValue,
+		`the request uses "([^"]+)" compression`:                               requestUsesCompression,
 		`the request is sent`:                                                  requestIsSent,
 		`the request with pagination is sent`:                                  requestWithPaginationIsSent,
 		`the response status is (\d+) (.*)`:                                    statusIs,
